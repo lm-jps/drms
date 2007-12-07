ad68ceab23911ac49871cf6043b09fd5d50748ec
cvs-fast-export <cvs-fast-export>
2007-12-07 21:25:27 +0000

    Synthetic commit for incomplete tag NetDRMS_Ver_0-7

--- projconf/su/make_basic.mk
+++
@@ -1,8 +0,0 @@
-# Make rules that apply to all projects
-
-$(CEXESUMS):			$(LIBSUMSAPI) $(LIBSUM)
-$(MODEXESUMS):			$(LIBSUMSAPI) $(LIBSUM)
-
-$(MODEXEDROBJ):			CF_TGT := $(CF_TGT) -I$(SRCDIR)/proj/libs/dr
-$(MODEXEDR) $(MODEXEDR_SOCK):	$(LIBDR)
-
