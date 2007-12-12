1596a20a3a65c542c18da55553e929a145e610c3
arta <arta>
2007-12-12 19:51:10 +0000

    Move cmdparams_parse documentation from man page to doxygen comments

--- base/libs/cmdparams/cmdparams.h
+++ base/libs/cmdparams/cmdparams.h
@@ -1,10 +1,17 @@
 /**
 @file cmdparams.h
 @brief Functions to get values in appropriate form from parsed command line and module arguments list.
-@sa ::module
-@bug If ::cmdparams_get_str (which is called by all the other versions of
+@sa ::module ::module_args
+@bug ::cmdparams_get_str - If ::cmdparams_get_str (which is called by all the other versions of
 \a cmdparams_get_XXX)cannot find a hash table entry for the requested
 name, it will use the corresponding environment variable, if it exists.
+@bug ::cmdparams_parse - Multiple assignments to a given parameter name 
+could result in unpredictable
+values. The last token in the command line should be parsed last and take
+precedence. Thus, for example, an assignment following an \@filename declaration
+will superseed the assignment in the corresponding file, and \a vice versa
+@bug ::cmdparams_parse - Parsing of tokens in included files does not properly deal with embedded
+white space in quoted strings.
 
 These functions return values of the appropriate type from the hash list
 of parameters expected to be available to a module as a global variable.
@@ -60,6 +67,77 @@ typedef struct CmdParams_t_struct {
   char *buffer;
 } CmdParams_t;
 
+/**
+   Parse command line and module arguments struct and create hashed parms list.
+   This function, ordinarily called by the ::jsoc_main or
+   other driver program, but accessible from any program, parses the command
+   line (or other array of strings described by \a argc and \a argv!), included files
+   if any, and the global ::module_args struct to create a hash table of all
+   arguments available in the \a parms struct. Parsing obeys the following rules:
+
+   1. All tokens after the first in the command line (\a argv) are parsed for
+   include file references, flags, or parameter name value pairs.
+
+   2. Included files, described by tokens of the form \a @filename in either
+   the command line or an included file, are parsed according to the same
+   rules as the command line.
+
+   3. The declared ::module_args list is scanned, and all named parameters
+   not already assigned values are assigned their declared defaults, if any.
+
+   If any declared module argument cannot be assigned a value by the above
+   rules, processing of the argument list ceases and the function returns
+   the value -1. The exception is for arguments declared to be of type
+   ::ARG_FLAG, which are simply not set.
+
+   All values in the parms struct are strings, regardless of the declared
+   type of the argument. For arguments of type ::ARG_NUME, the value from
+   parsing will be string-compared with the set of valid strings in the
+   argument range, and then set to the character representation of the
+   numeric order of the (first) matching value in the range, counting from
+   0.
+
+   Command-line tokens can take any of the following forms:
+   <TABLE>
+   <TR>
+   <TD>\a --name</TD> 
+   <TD>(name cannot begin with a digit)</TD>
+   </TR>
+   <TR>
+   <TD>\a -chars</TD>
+   <TD>where chars is a set of one or more characters, all of which are
+   set as key names with the value "1"</TD>
+   </TR>
+   <TR>
+   <TD>\a name=[]value</TD>
+   <TD>white space following the \a = is optional</TD>
+   </TR>
+   <TR>
+   <TD>\a \@filename</TD>
+   <TD>specifies name of file containing tokens to be parsed following same rules
+   as command line</TD>
+   </TR>
+   <TR>
+   <TD>\a value</TD>
+   <TD>assigned to successive unnamed argument $n</TD>
+   </TR>
+   </TABLE>
+
+   If a parameter value (or name) has embedded white space, it must be quoted.
+
+   An extern ::module_args declaration is required, though it can be empty.
+   A member with a ::module_args.type of ::ARG_END, must terminate the
+   the list of parsed members; any members following it in the declarator
+   will be ignored.
+*/
+int cmdparams_parse (CmdParams_t *parms, int argc,  char *argv[]);
+
+int cmdparams_parsefile (CmdParams_t *parms, char *filename, int depth);
+int cmdparams_exists (CmdParams_t *parms, char *name);
+void cmdparams_printall (CmdParams_t *parms);
+char *cmdparams_getarg (CmdParams_t *parms, int num);
+void cmdparams_usage (char *name);
+int cmdparams_numargs (CmdParams_t *parms);
 
 /**
 @name [cmd]params_isflagset
@@ -194,14 +272,5 @@ void f_params_isflagset(pFHandleCmdParams handle, const char *name, int *ret);
 /* @} */
 
 #endif /* FLIB */
-
-/* Used by server apps */
-int cmdparams_parse (CmdParams_t *parms, int argc, char **argv);
-int cmdparams_parsefile (CmdParams_t *parms, char *filename, int depth);
-int cmdparams_exists (CmdParams_t *parms, char *name);
-void cmdparams_printall (CmdParams_t *parms);
-char *cmdparams_getarg (CmdParams_t *parms, int num);
-void cmdparams_usage (char *name);
-int cmdparams_numargs (CmdParams_t *parms);
 
 #endif
