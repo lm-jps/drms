2d1c1e0306266da25c66b91495a9eff2130a5399
arta <arta>
2008-02-12 03:29:11 +0000

    Make include path have 'gsl' prefix

--- projconf/su/make_basic.mk
+++ projconf/su/make_basic.mk
@@ -6,8 +6,7 @@ $(MODEXESUMS):			$(LIBSUMSAPI) $(LIBSUM)
 $(MODEXEDROBJ):			CF_TGT := $(CF_TGT) -I$(SRCDIR)/proj/libs/dr
 $(MODEXEDR) $(MODEXEDR_SOCK):	$(LIBDR)
 
-$(GSLOBJ):			CF_TGT := $(CF_TGT) -I$(_JSOCROOT_)/lib_third_party/include/gsl/
-
+$(GSLOBJ):			CF_TGT := $(CF_TGT) -I$(_JSOCROOT_)/lib_third_party/include/ 
 
 ifeq ($(COMPILER), icc)
   ifeq ($(JSOC_MACHINE), linux_x86_64)
