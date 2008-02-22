ebd28226d1b98be28a878ab174605ed21dff47d0
phil <phil>
2008-02-22 19:04:54 +0000

    added drms_print_rec_query

--- base/drms/libs/api/drms_parser.h
+++ base/drms/libs/api/drms_parser.h
@@ -3,7 +3,13 @@
 
 DRMS_Record_t *drms_parse_description(DRMS_Env_t *env, char *desc);
 int parse_keywords(char *desc, DRMS_Record_t *template);
+
+/* print jsd from existing series */
 void drms_jsd_printfromrec(DRMS_Record_t *rec);
 void drms_jsd_print(DRMS_Env_t *drms_env, const char *seriesname);
+
+/* print a query that will return the given record */
+void drms_print_rec_query(DRMS_Record_t *rec);
+void drms_fprint_rec_query(FILE *fp, DRMS_Record_t *rec);
 
 #endif
