2d0b1bbe73085e4d48513ec781d558e62d74dd2b
cvs-fast-export <cvs-fast-export>
2008-02-27 20:17:01 +0000

    Synthetic commit for incomplete tag NewTree01_cp08

--- base/drms/libs/api/drms_binfile.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DRMS_BINFILE_H
-#define __DRMS_BINFILE_H
-
-int drms_binfile_read(char *filename, int nodata, DRMS_Array_t *ar);
-int drms_binfile_write(char *filename, DRMS_Array_t *arr);
-int drms_zipfile_read(char *filename, int nodata,DRMS_Array_t *ar);
-int drms_zipfile_write(char *filename, DRMS_Array_t *rf);
-
-#endif
