e263ebe02b596b7ec3a29804d50ac57a2fd68bb5
rick <rick>
2008-03-04 23:42:12 +0000

    added generic GCC make rule so that there is something even if $JSOC_MACHINE is generic or unrecognized; it happens to be identical to the rule for the linux_ia64

--- make_basic.mk
+++ make_basic.mk
@@ -108,14 +108,16 @@ endif
 
 # can't figure out how to get stupid make to do if/else if/else
 ifeq ($(DEBUG), 0)
-
-  ICC_CF_ALL	= -I$(SRCDIR)/base/include -std=c99 -xW $(ICC_WARN) $(ICC_CF_ICCCOMP)
+# defaults for generic architecture
+  GCC_CF_ALL = -I$(SRCDIR)/base/include -std=gnu99 -O2 $(GCC_WARN) $(GCC_CF_GCCCOMP)
+  ICC_CF_ALL = -I$(SRCDIR)/base/include -std=c99 -xW $(ICC_WARN) $(ICC_CF_ICCCOMP)
 
   ifeq ($(JSOC_MACHINE), linux_x86_64)
     GCC_CF_ALL	= -I$(SRCDIR)/base/include -std=gnu99 -O2 -march=opteron $(GCC_WARN) $(GCC_CF_GCCCOMP)
   endif
 
   ifeq ($(JSOC_MACHINE), linux_ia64)
+# these are superfluous, since they are identical to the generic defines
     GCC_CF_ALL	= -I$(SRCDIR)/base/include -std=gnu99 -O2 $(GCC_WARN) $(GCC_CF_GCCCOMP)
     ICC_CF_ALL	= -I$(SRCDIR)/base/include -std=c99 $(ICC_WARN) $(ICC_CF_ICCCOMP)
   endif
@@ -126,8 +128,8 @@ ifeq ($(DEBUG), 0)
 
 else
 
-  GCC_CF_ALL	= -I$(SRCDIR)/base/include -std=gnu99 -g $(GCC_WARN) $(GCC_CF_GCCCOMP)
-  ICC_CF_ALL	= -I$(SRCDIR)/base/include -std=c99 -g $(ICC_WARN) $(ICC_CF_ICCCOMP)
+  GCC_CF_ALL = -I$(SRCDIR)/base/include -std=gnu99 -g $(GCC_WARN) $(GCC_CF_GCCCOMP)
+  ICC_CF_ALL = -I$(SRCDIR)/base/include -std=c99 -g $(ICC_WARN) $(ICC_CF_ICCCOMP)
 
 endif
 
