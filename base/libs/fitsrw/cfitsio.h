68aa17b5e1d4fcd656f6f28aa161d2bfb836e31a
arta <arta>
2008-03-06 18:11:40 +0000

    Integrate Tim's latest changes into CVS.  Also, added a couple of function prototypes for Tim to implement.

--- base/libs/fitsrw/cfitsio.h
+++ base/libs/fitsrw/cfitsio.h
@@ -1,10 +1,13 @@
+//****************************************************************************
 
 #ifndef __CFITSIO__
 #define __CFITSIO__
 
+#include <fitsio.h>
+
 //****************************************************************************
 
-#define CFITSIO_MAX_DIM							9
+#define CFITSIO_MAX_DIM		          9
 
 #define CFITSIO_SUCCESS                   0
 #define CFITSIO_FAIL                     -1
@@ -31,12 +34,12 @@ typedef union cfitsio_value
 
 typedef struct cfitsio_keyword
 {
-	char				key_name[FLEN_KEYWORD]; 
-	char				key_type; // C: string, L: logical, I: integer, F: float, X: complex 
+	char			key_name[FLEN_KEYWORD]; 
+	char			key_type; // C: string, L: logical, I: integer, F: float, X: complex 
 	CFITSIO_KEY_VALUE	key_value;
-	char				key_comment[FLEN_COMMENT];
-	char				key_printf_format[FLEN_KEYWORD];
-	void*				next; // For fixed card size, just access as array element and return "nkeys"
+	char			key_comment[FLEN_COMMENT];
+	char			key_printf_format[FLEN_KEYWORD];
+	void*			next; // For fixed card size, just access as array element and return "nkeys"
 
 } CFITSIO_KEYWORD;
 
@@ -52,29 +55,47 @@ typedef	struct cfitsio_image_info
 
 
 typedef enum cfitsio_compression_type 
-	{C_NONE, C_IMAGE, C_ROW, C_EXTENDED_FILENAME} CFITSIO_COMPRESSION_TYPE;
+{
+	C_NONE,			// not compress
+	C_DEFAULT,		// rice algorithm, compress row_by_row
+	C_RICE,			// 
+	C_HCOMPRESS,		// hcompress algorithm, whole image
+	C_GZIP,			// gzip algorithm
+	C_PLIO,			// plio algorithm
+	C_EXTENDED_FILENAME	// user specified using extended filename 
+} CFITSIO_COMPRESSION_TYPE;
 
 //****************************************************************************
 
 int cfitsio_read_keys(char* filename, CFITSIO_KEYWORD** list);
 int cfitsio_read_sum_keys(char* filename, CFITSIO_KEYWORD** list);
 int cfitsio_print_keys(CFITSIO_KEYWORD* list);
+void cfitsio_free_keys(CFITSIO_KEYWORD** list);
 
 int cfitsio_read_header(char* filename, char** header);
 int cfitsio_print_header(char* filename);
 
 int cfitsio_read_image(char* filename, void** image);
 int cfitsio_dump_image(void* image, CFITSIO_IMAGE_INFO* info, 
-							  long from_row, long to_row, long from_col, long to_col);
+			  long from_row, long to_row, long from_col, long to_col);
+void cfitsio_free_image(void **image);
 
-int cfitsio_write_file(char* fits_filename, char* header,  void* image);
+int cfitsio_read_file(char* filename, CFITSIO_KEYWORD** list, void** image);
+void cfitsio_free_keysandimg(CFITSIO_KEYWORD** list, void** image);
+
+int cfitsio_write_file(char* fits_filename, char* header , void* image, 
+			CFITSIO_COMPRESSION_TYPE compression_type);
+int cfitsio_pure_write_file(char* fits_filename, char* header,  void* image);
 int cfitsio_get_image_info(char* header, CFITSIO_IMAGE_INFO* info);
 
 void cfitsio_print_error(int status);
 
-int cfitsio_write_compress(char* fits_filename, char* header ,
-						   void* image, 
-						   CFITSIO_COMPRESSION_TYPE compression_type);
+
+//-------------------------
+
+int cfitsio_write_compress(char* fits_filename, char* header , void* image, 
+				CFITSIO_COMPRESSION_TYPE compression_type);
+int cfitsio_gen2d_image(char* fits_filename, long width, long height);
 
 //****************************************************************************
 
--- base/libs/fitsrw/test/test1.c
+++ base/libs/fitsrw/test/test1.c
@@ -1,68 +1,57 @@
+//****************************************************************************
+// CFITSIO.C
+//
+// Simple test code demo the use of CFITSIO.
+// 
+//
+//****************************************************************************
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "fitsio.h"
-#include "../cfitsio.h"
+#include "cfitsio.h"
 
 //****************************************************************************
-//Simple test code
+
 
 int main()
 {
-	CFITSIO_KEYWORD*	key  = NULL;
-	void*	image = NULL;
-	char* header = NULL;
 
-	cfitsio_read_keys("image_1.fits", &key); 
-	cfitsio_print_keys(key);
-	
-	cfitsio_read_header("hmi_image_1.fits", &header);
-	cfitsio_print_header(header);
+   CFITSIO_KEYWORD* keys  = NULL;
+   void* image = NULL;
+   char* header = NULL;
 
-	cfitsio_read_image("hmi_image_1.fits", &image); 
-	cfitsio_write_file("hmi_image_1_out.fits", header, image);
-
-	cfitsio_write_compress("hmi_image_1_rice.fits", header, image, C_ROW);
-	cfitsio_write_compress("hmi_image_1_hcomp.fits", header, image, C_IMAGE);
+   // Read keywords and values from a fits file
+   cfitsio_read_keys("image_1.fits", &keys); 
+   cfitsio_print_keys(keys);
 
 
+   // Read Header from a fits file
+   cfitsio_read_header("hmi_image_1.fits", &header);
+   cfitsio_print_header(header);
+
+   // Read Image from a fits file
+   cfitsio_read_image("hmi_image_1.fits", &image); 
 
 
 
-	/*
-	cfitsio_write_compress("hmi_image_1_out_h.fits", key, image, C_IMAGE);
-	cfitsio_write_compress("hmi_image_1_out_r.fits", key, image, C_ROW);
-	*/
+   // Write none_compress file
+   cfitsio_write_file("hmi_image_1_out.fits", header, image, C_NONE);
+
+   // Default compress file use Rice algo and compress row by row
+   cfitsio_write_file("hmi_image_1_defautl.fits", header, image, C_DEFAULT);
+
+   // Write default compress file using Rice algo and compress row by row
+   cfitsio_write_file("hmi_image_1_r.fits", header, image, C_RICE);
+
+   // Write a compress file using Rice algo, title size 100x100
+   cfitsio_write_file("hmi_image_1_c.fits[compress R 100, 100]", header, image, C_EXTENDED_FILENAME);
 
 
-	/*
-	cfitsio_read_keys("image_1.fits", &key); 
-	cfitsio_print_keys(key);
+   if(header)	free(header);
+   if(image)	free(image);
+   if(keys)	free(keys); 
 
-	cfitsio_read_image("image_1.fits", &image); 
-	
-	cfitsio_write_file("image_1_out.fits", key, image);
-	
-	cfitsio_write_compress("image_1_out_h.fits", key, image, C_IMAGE);
-	cfitsio_write_compress("image_1_out_r.fits", key, image, C_ROW);
-	*/
-
-	//cfitsio_read_image("hmi_image_1.fits", &image);
-	//cfitsio_write("hmi_image_1_none.fit", header , nkeys, image);
-
-	//cfitsio_write_compress_3("hmi_image_1_row.fit",
-	//	header,nkeys,image,C_ROW);
-
-	//cfitsio_write_compress_4("hmi_image_1_none.fit", header , nkeys, image, C_NONE);
-
-	//cfitsio_write_compress_2("image_1_file_ext.fit[compress]",header,nkeys,image,C_EXTENDED_FILENAME);
-
-
-	if(header)	free(header);
-	if(image)	free(image);
-	if(key)		free(key); 
-
-    return(0);
+   return(0);
 }
-//****************************************************************************
-//****************************************************************************
