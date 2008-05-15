0fab0a21b07dc55441a589f56234a8d7af764740
production <production>
2008-05-15 19:13:28 +0000

    add SUMPGPORT

--- base/sums/libs/api/SUM.h
+++ base/sums/libs/api/SUM.h
@@ -4,18 +4,21 @@
 #define SUM_VERSION_NUM    (1.0)
 #define SUMSERVER "dcs0.Stanford.EDU"
 #define SUMDB "dcs0"
+#define SUMPGPORT "5432"
 #define TAPEVIEWERNAME "t50view"
 #define SUM_STOP_NOT "/usr/local/logs/SUM/SUM_STOP_NOT"
 #elif defined SUMT120
 #define SUM_VERSION_NUM    (1.0)
 #define SUMSERVER "d00.Stanford.EDU"
 #define SUMDB "hmidb"
+#define SUMPGPORT "5434"
 #define TAPEVIEWERNAME "t120view"
 #define SUM_STOP_NOT "/usr/local/logs/SUM/SUM_STOP_NOT"
 #elif defined SUMT950
 #define SUM_VERSION_NUM    (1.0)
 #define SUMSERVER "d02.Stanford.EDU"
 #define SUMDB "hmidb"
+#define SUMPGPORT "5434"
 #define TAPEVIEWERNAME "t950view"
 #define SUM_STOP_NOT "/usr/local/logs/SUM/SUM_STOP_NOT"
 #endif
