04259fc0f4e0c2646704f58903012466c447c97a
production <production>
2008-06-04 18:08:32 +0000

    update

--- base/sums/scripts/build_parc_file.pl
+++ base/sums/scripts/build_parc_file.pl
@@ -29,7 +29,9 @@ sub usage {
 }
 
 #Return date in form e.g. 2008-05-29 11:38:18
-#where this value is 24 hours previous to the current time.
+#where this value is $hrsprev previous to the current time.
+#NOTE: this format must match that in the jsoc_sums DB
+#Also sets $currdate to current time of form 2008-05-29_11:38:18
 sub labeldate {
   local($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst,$date,$sec2,$min2,$hour2,$mday2,$year2);
   $secprev = $hrsprev*3600;
@@ -41,15 +43,24 @@ sub labeldate {
   $mon2 = sprintf("%02d", $mon+1);
   $year4 = sprintf("%04d", $year+1900);
   $date = $year4."-".$mon2."-".$mday2." ".$hour2.":".$min2.":".$sec2;
+  ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+  $sec2 = sprintf("%02d", $sec);
+  $min2 = sprintf("%02d", $min);
+  $hour2 = sprintf("%02d", $hour);
+  $mday2 = sprintf("%02d", $mday);
+  $mon2 = sprintf("%02d", $mon+1);
+  $year4 = sprintf("%04d", $year+1900);
+  $currdate = $year4."-".$mon2."-".$mday2."_".$hour2.":".$min2.":".$sec2;
   return($date);
 }
 
-$HOSTDB = "hmidb";     #db machine
-$HOSTDC = "dcs0";      #default datacapture to send to
-$TLMDS = "hmi.tlm_60d"; #series of .tlm files to query
+$HOSTDB = "hmidb";	#db machine
+$HOSTDC = "dcs0";	#default datacapture to send to
+$TLMDSHMI = "hmi.tlm_60d"; #series of .tlm files to query
+$TLMDSAIA = "aia.tlm_60d"; #series of .tlm files to query
 $DB = "jsoc_sums";
 $PGPORT = 5434;
-$parcfile = "/tmp/test.parc";
+$PARC_ROOT = "/usr/local/logs/parc/";
 $hrsprev = 24;
 while ($ARGV[0] =~ /^-/) {
   $_ = shift;
@@ -66,7 +77,9 @@ while ($ARGV[0] =~ /^-/) {
 }
 
 $ldate = &labeldate();
-print "Going to find tlm data archived since $ldate\n";
+$parchmi = $PARC_ROOT."HMI_$currdate".".parc";
+$parcaia = $PARC_ROOT."AIA_$currdate".".parc";
+print "Going to find tlm data archived since $ldate\n\n";
 
 $hostdb = $HOSTDB;      #host where Postgres runs
 $user = $ENV{'USER'};
@@ -82,27 +95,32 @@ if($user ne "production") {
     die "Cannot do \$dbh->connect: $DBI::errstr\n";
   }
 
-    $sql = "select online_loc, arch_tape, arch_tape_fn, arch_tape_date from sum_main where Arch_Tape_Date >= '$ldate' and Owning_Series = '$TLMDS'";
+for($i = 0; $i < 2; $i++) { 	#do first for HMI then AIA
+  if($i == 0) {
+    open(PARC, ">$parchmi") || die "Can't open $parchmi: $!\n";
+    $resultfile = $parchmi; $instru = "HMI";
+    $sql = "select online_loc, arch_tape, arch_tape_fn, arch_tape_date from sum_main where Arch_Tape_Date >= '$ldate' and Owning_Series = '$TLMDSHMI'";
+  } else {
+    open(PARC, ">$parcaia") || die "Can't open $parcaia: $!\n";
+    $resultfile = $parcaia; $instru = "AIA";
+    $sql = "select online_loc, arch_tape, arch_tape_fn, arch_tape_date from sum_main where Arch_Tape_Date >= '$ldate' and Owning_Series = '$TLMDSAIA'";
+  }
+    print PARC "#Created by build_parc_file.pl for query past $ldate\n";
+    print PARC "#owning_series_name                         tape_id  fn  date\n";
+
     $sth = $dbh->prepare($sql);
     if ( !defined $sth ) {
       print "Cannot prepare statement: $DBI::errstr\n";
       $dbh->disconnect();
       exit; 
     }
-
-    open(PARC, ">$parcfile") || die "Can't open $parcfile: $!\n";
-    print PARC "#Created by build_parc_file.pl for query past $ldate\n";
-    print PARC "#owning_series_name                         tape_id  fn  date\n";
-
     # Execute the statement at the database level
     $sth->execute;
     # Fetch the rows back from the SELECT statement
     @row = ();
-    $found = 0;
+    $count = 0;
     while ( @row = $sth->fetchrow() ) {
       $dir = shift(@row);
-
-@xx = `/bin/ls $dir/S00000`;
       if(!opendir(DIR, "$dir/S00000")) {
         print "Can't open $dir\n";
         next;
@@ -113,21 +131,18 @@ if($user ne "production") {
       while($file = shift(@dirfiles)) {
         if(($pos = rindex($file, ".tlm")) != -1) { 
           $found = 1;
+          $count++;
           last;
         }
       }
       if(!$found) { next; }
       $pos = rindex($file, '.');
       $fftlm = substr($file, 0, $pos);
-
-#      $file = `/bin/ls $dir/S00000/*.tlm`;
-#      $pos = index($file, "VC0");
-#      $ftlm = substr($file, $pos);
-#      $pos = rindex($ftlm, '.');
-#      $fftlm = substr($ftlm, 0, $pos);
       print PARC "$fftlm @row\n";
     }
-print "All done\n";
+  close(PARC);
+  print "Done: Found $count tlm files for $instru\n";
+  print "Results in $resultfile\n\n";
+}
 $dbh->disconnect();
-close(PARC);
 
