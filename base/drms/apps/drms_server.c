bb47717e03a92fd849f0c8ed22b36f09386ddd6d
karen <karen>
2008-06-26 22:56:39 +0000

    fixed a string const

--- base/drms/apps/drms_server.c
+++ base/drms/apps/drms_server.c
@@ -279,7 +279,7 @@ int main (int argc, char *argv[]) {
 
   env->dbpasswd = dbpasswd;
   env->user = user;
-  env->logfile_prefix = drms_server;
+  env->logfile_prefix = "drms_server";
   env->dolog = dolog;
   env->quiet = 1;
 
