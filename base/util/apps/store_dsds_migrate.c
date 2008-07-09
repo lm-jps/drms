fcaff7e12ed70aeadd97e5c29787ed8aa5b90f7f
production <production>
2008-07-09 23:13:51 +0000

    create series if doesn't exist. ingest directly into final series.

--- base/util/apps/store_dsds_migrate.c
+++ base/util/apps/store_dsds_migrate.c
@@ -5,16 +5,18 @@
  *                                  snum=<number> dirname=<dir> {note=<comment>}
  *         
  *     Store a dir into SUMS for a DSDS dataset of the given name.
- *     "dir" is the directory that will be copied in SUMS
- *     in the dataseries su_production.dsds_migrate.  
+ *     "dir" is the directory that will be copied in SUMS.
  *
- *     Multiple calls of store_dsds_migrate with the same 
- *     names and numbers will result in
- *     multiple records with the same prime key.  Only the most recent
- *     will be easily retrieved. 
+ * The DRMS series is in name space 'dsds'. So, for example, the dsds ds
+ * prog:mdi,level:lev1.5[4],series:fd_V_01h[60000] will be ingested into:
+ *
+ * dsds.mdi__lev1_5__fd_V_01h
+ *
+ * where PrimeKeys in the .jsd is the series#, and the level# is a DBIndex.
+ * Note the double '__' to separate the fields.
  *
  * Example:
- *   store_dsds_migrate pname=mdi lname=lev1.5 lnum=2 sname=fd_V_01h snum=60000
+ *   store_dsds_migrate pname=mdi lname=lev1.5 lnum=4 sname=fd_V_01h snum=60000
  *			dirname=/PDS83/D9666638
  *
  */
@@ -36,6 +38,7 @@ ModuleArgs_t module_args[] = {
   {ARG_STRING, "dirname", "", "Dir to store into SUMS"},
   {ARG_STRING, "note", "N/A", "comment field"},
   {ARG_FLAG, "v", "0", "verbose flag"},
+  {ARG_FLAG, "j", "0", "print jsd flag"},
   {ARG_END}
 };
 
@@ -46,6 +49,7 @@ char *module_name = "store_dsds_migrate";
 
 /* Some global variables for this module. */
 int verbose = 0;
+int jsdflg = 0;
 
 /* Check DRMS session status and calling arguments and set verbose variable */
 int nice_intro () {
@@ -59,14 +63,16 @@ int DoIt (void) {
 int status = 0;
 int lnum, snum;
 
-char *in, *pname, *lname, *sname, *note, *dirname;
+char *in, *pname, *lname, *sname, *note, *dirname, *cptr;
 char path[DRMS_MAXPATHLEN];
+char drmsseries[256];
 char cmd[DRMS_MAXPATHLEN+1024];
 DRMS_Record_t *rec, *template;
 
 if (nice_intro())
   return(0);
 
+jsdflg = cmdparams_get_int (&cmdparams, "j", NULL);
 pname = cmdparams_get_str(&cmdparams, "pname", NULL);
 lname = cmdparams_get_str(&cmdparams, "lname", NULL);
 sname = cmdparams_get_str(&cmdparams, "sname", NULL);
@@ -82,11 +88,39 @@ if (access(in, R_OK) != 0)
   }
   dirname = in;
 
+/* Check to see if series exists, and if we should make it */
+cptr = (char *)index(lname, '.'); //no dsds lname has more then one '.'
+if(cptr) { 
+  *cptr = '_';		//replace '.' with '_'
+}
+sprintf(drmsseries, "dsds.%s__%s__%s", pname, lname, sname);
+if(jsdflg) {
+  printf("%s", make_series_jsd(drmsseries));
+  return(1);
+}
+template = drms_template_record(drms_env, drmsseries, &status);
+if (template==NULL && status == DRMS_ERROR_UNKNOWNSERIES)
+  {
+    int create_series(char *series);
+    printf("Going to make series: %s\n", drmsseries);
+    if (create_series(drmsseries))
+      {
+      printf("Series '%s' does not exist. Create_series failed. Give up.\n", drmsseries);
+      return(1);
+      }
+  }
+else
+  if(status)
+    {
+    fprintf(stderr, "DRMS problem looking up drmsseries.\n");
+    return(status);
+    }
+
 /* Now ready to make a new record and set keywords */
-rec = drms_create_record(drms_env, outputseries, DRMS_PERMANENT, &status);
+rec = drms_create_record(drms_env, drmsseries, DRMS_PERMANENT, &status);
 if (!rec || status)
     {
-    printf("drms_create_record failed, series=%s, status=%d.  Aborting.\n", outputseries,status);
+    printf("drms_create_record failed, series=%s, status=%d.  Aborting.\n", drmsseries,status);
     return(status);
     }
 if ((status = drms_setkey_string(rec, "dirname", dirname)))
@@ -94,24 +128,9 @@ if ((status = drms_setkey_string(rec, "dirname", dirname)))
      printf("ERROR: drms_setkey_string failed for 'dirname'\n");
      return(1);
    }
-if ((status = drms_setkey_string(rec, "pname", pname)))
-   {
-     printf("ERROR: drms_setkey_string failed for 'pname'\n");
-     return(1);
-   }
-if ((status = drms_setkey_string(rec, "lname", lname)))
-   {
-     printf("ERROR: drms_setkey_string failed for 'lname'\n");
-     return(1);
-   }
 if ((status = drms_setkey_int(rec, "lnum", lnum)))
    {
      printf("ERROR: drms_setkey_int failed for 'lnum'\n");
-     return(1);
-   }
-if ((status = drms_setkey_string(rec, "sname", sname)))
-   {
-     printf("ERROR: drms_setkey_string failed for 'sname'\n");
      return(1);
    }
 if ((status = drms_setkey_int(rec, "snum", snum)))
@@ -146,3 +165,53 @@ if ((status = drms_close_record(rec, DRMS_INSERT_RECORD)))
 return(status);
 }
 
+char * make_series_jsd(char *series)
+  {
+  char *user = getenv("USER");
+  char jsd[2048];
+  sprintf(jsd,
+    "Seriesname:     %s\n"
+    "Author:         %s\n"
+    "Owners:         %s\n"
+    "Unitsize:       1\n"
+    "Archive:        0\n"
+    "Retention:      270\n"
+    "Tapegroup:      9\n"
+    "PrimeKeys:      snum\n"
+    "DBIndex:        snum,lnum\n"
+    "Description:    \"DSDS migrated dataset\"\n"
+    "Keyword: dirname,  string, variable, record, \" \",  %%s, none, \"Original dirname\"\n"
+    "Keyword: note,      string, variable, record, \" \",  %%s, none, \" \"\n"
+    "Keyword: lnum,      int, variable, record, \"0\",  %%d, none, \"level: number\"\n"
+    "Keyword: snum,      int, variable, record, \"-1\",  %%d, none, \"series: number\"\n"
+    "Data: dir, constant, int, 0, none, generic, \" \"\n",
+    series, user, user);
+  return (strdup(jsd));
+  }
+
+
+int create_series(char *series)
+  {
+  char *jsd;
+  int perm = cmdparams_get_int(&cmdparams, "perm", NULL);
+  DRMS_Record_t *template;
+  jsd = make_series_jsd(series);
+  template = drms_parse_description(drms_env, jsd);
+  if (template==NULL)
+    {
+    fprintf(stderr, "Failed to parse series description.  JSD was:\n%s\n",jsd);
+    return(1);
+    }
+  if (drms_create_series(template, perm))
+    {
+    fprintf(stderr, "Failed to create series. JSD was:\n%s\n", jsd);
+    return(1);
+    }
+  if (verbose)
+    printf("Create_series successful, JSD was:\n%s\n", jsd);
+  drms_free_record_struct(template);
+  free(template);
+  free(jsd);
+  return(0);
+  }
+  
