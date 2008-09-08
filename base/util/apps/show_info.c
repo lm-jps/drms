98d27914d71a963bcae86675122098c4a9c0c0a0
arta <arta>
2008-09-08 22:43:51 +0000

    Ticket #80 - When the -P flag AND the -A (or seg=XXX) are set, ensure that the retrieve flag is not set

--- base/util/apps/show_info.c
+++ base/util/apps/show_info.c
@@ -689,8 +689,15 @@ int DoIt(void)
   free (seglist);
 
   /* if some segment info wanted and path is needed from SUMS, stage all records. */
-  if ((nsegs > 0 && (want_path || want_path_noret)) || want_path || want_path_noret)
-    drms_stage_records(recordset, want_path, want_path); 
+  if (nsegs > 0)
+  {
+     if (want_path)
+       /* -p */
+       drms_stage_records(recordset, 1, 0); 
+     else if (want_path_noret)
+       /* -P */
+       drms_stage_records(recordset, 0, 1); 
+  }
 
   /* loop over set of selected records */
   for (irec = first_rec; irec <= last_rec; irec++) 
