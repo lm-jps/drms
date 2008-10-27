ffae5b51aec9c0c04e70cae85b38625816bdd568
jsoc <jsoc>
2008-10-27 18:57:08 +0000

    Fix more permission issues with the nightly build script.

--- build/jsoc_build.pl
+++ build/jsoc_build.pl
@@ -4,24 +4,37 @@ use strict;
 use warnings;
 use Data::Dumper;
 
+my($TESTERR) = "no";
 my($JSOC_DEV) = "jsoc_dev\@sun.stanford.edu";
-# my($JSOC_DEV) = "arta\@sun.stanford.edu"; # for testing purposes
+#my($JSOC_DEV) = "arta\@sun.stanford.edu"; # for testing purposes
 my($ROOTDIR) = "/tmp/jsoc";
 my($MAKELOG) = "make.log";
+my($ret);
 
-$ENV{'CVSROOT'} = ':ext:lws.stanford.edu:/home/cvsuser/cvsroot';
-$ENV{'CVS_RSH'} = 'rsh';
+$ENV{'CVSROOT'} = ':ext:sunroom.stanford.edu:/home/cvsuser/cvsroot';
+$ENV{'CVS_RSH'} = 'ssh';
 $ENV{'PATH'} .= ':/usr/local/bin';
 
-my $cmd = "cd $ROOTDIR; rm -rf JSOC; cvs co JSOC; cvs update JSOC; cd JSOC; ./configure";
+if ($TESTERR eq "no")
+{
+    if (-e "$ROOTDIR/JSOC")
+    {
+        `rm -rf "$ROOTDIR/JSOC" 2>&1 1>>/dev/null`;
+    }
 
-$cmd = join("; ", map {
-    $_." 2>&1 1>/dev/null";
-} split("; ", $cmd));
+    `mkdir -p "$ROOTDIR/JSOC" 2>&1 1>>/dev/null`;
 
-`$cmd; make universe -k >& $MAKELOG`;
+    my $cmd = "cd $ROOTDIR; cvs co JSOC; cvs update JSOC; cd JSOC; ./configure";
+    $cmd = join("; ", map {
+        $_." 2>&1 1>>$ROOTDIR/JSOC/cmd.log";
+    } split("; ", $cmd));
 
-# `cd $ROOTDIR/JSOC; make universe -k >& $MAKELOG`;
+    $ret = `$cmd; make universe -k >& $ROOTDIR/JSOC/$MAKELOG`;
+}
+else
+{
+    `cd $ROOTDIR/JSOC; make universe -k >& $MAKELOG`;
+}
 
 open FH, "cd $ROOTDIR/JSOC; \(tail -n 4 $MAKELOG | grep Error\)  |" || die "Can't open log file: $!\n";
 my @line = <FH>;
