46235d9e12431ae45241dc9870fef105ee45edaf
production <production>
2008-11-26 17:33:07 +0000

    updated char size

--- base/sums/libs/api/sum_info.h
+++ base/sums/libs/api/sum_info.h
@@ -1,24 +1,26 @@
 #ifndef __SUM_INFO_H
 #define __SUM_INFO_H
 
-//All the info is from sum_main db table for the sunum
+//All the info is from sum_main db table for the sunum.
+//The char sizes are from the sum_main DB table schema +1 where
+//content may be full size.
 typedef struct SUM_info_struct
 {
   uint64_t sunum;		//aka ds_index
-  char online_loc[80];
+  char online_loc[81];
   char online_status[5];
   char archive_status[5];
   char offsite_ack[5];
-  char history_comment[80];
-  char owning_series[80];
+  char history_comment[81];
+  char owning_series[81];
   int storage_group;
   double bytes;
   char creat_date[32];
-  char username[10];
-  char arch_tape[20];
+  char username[11];
+  char arch_tape[21];
   int arch_tape_fn;
   char arch_tape_date[32];
-  char safe_tape[20];
+  char safe_tape[21];
   int safe_tape_fn;
   char safe_tape_date[32];
 } SUM_info_t;
