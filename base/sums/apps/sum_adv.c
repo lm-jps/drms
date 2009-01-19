abee46e2b8b48888557d622608b2f21d0494dcb6
production <production>
2009-01-19 19:58:15 +0000

    add usage()

--- base/sums/apps/sum_adv.c
+++ base/sums/apps/sum_adv.c
@@ -1,7 +1,5 @@
 /* sum_adv.c
  * Queries the sum_svc.
- * This returns an exit(1) if it is now safe to shutdown the sum_svc
- * via a sum_stop.
  * Prints out 0 if a user is still opened in SUMS and you
  * should wait for it to close before you shut down SUMS.
  * Else prints 1.
@@ -14,6 +12,18 @@
 #include <rpc/rpc.h>
 #include <sum_rpc.h>
 
+void usage() 
+{
+ printf("Queries the sum_svc.\n");
+ printf("Prints out 0 if a user is still opened in SUMS and you\n");
+ printf("should wait for it to close before you shut down SUMS.\n");
+ printf("Else prints 1.\n");
+ printf("Normal call will turn off new SUM_open() calls in sum_svc.\n");
+ printf("Call with -q to not turn off new SUM_open() or to reenable if\n");
+ printf("already turned off.\n");
+ exit(0);
+}
+
 int main(int argc, char *argv[])
 {
   int shutmode, c;
@@ -25,7 +35,11 @@ int main(int argc, char *argv[])
       case 'q':
         queryonly=1;	//don't turn off/reenable new SUM_open() in sum_svc
         break;
+      case 'h':
+        usage();
+        break;
       default:
+        usage();
         break;
       }
     }
