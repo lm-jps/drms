146298b298b9f95cfd105d11a67ddee50a837192
production <production>
2009-02-10 00:24:39 +0000

    add doxygen

--- base/sums/apps/sum_adv.c
+++ base/sums/apps/sum_adv.c
@@ -1,3 +1,39 @@
+/**
+\defgroup sum_adv sum_adv - advise if sum_svc has an active user
+@ingroup sum_adv
+
+\brief This will query the sum_svc to determine if any user is still
+active, i.e. has done a SUM_open() but not a SUM_close().
+This is normally used by the sum_stop script to determine if the sum_svc
+can be shut down safely. 
+Prints out 0 if a user is still opened in SUMS and you
+should wait for it to close before you shut down SUMS.
+Else prints 1.
+Normal call will turn off new SUM_open() calls in sum_svc.
+Call with -q to not turn off new SUM_open() or to reenable if
+already turned off.
+
+\par Synopsis:
+
+\code
+sum_adv [-h] [-q]
+\endcode
+
+\par Flags:
+\c -h: print out help message<br>
+\c -q: do not disable new SUM_open() calls, or re-enable if already turned off.
+<br>
+\param
+none
+<br>
+\par NOTES:
+<pre>
+Calls the SUMS API function SUM_shutdown(), which calls sum_svc and
+receives a 0 or 1 answer back. The sum_stop_d02 calls sum_adv in order to
+advise the caller if the SUMS can be safely taken down.
+</pre>
+*/
+
 /* sum_adv.c
  * Queries the sum_svc.
  * Prints out 0 if a user is still opened in SUMS and you
