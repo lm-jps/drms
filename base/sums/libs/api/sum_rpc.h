ff3f3aa20b0fa82b3cecdf495121d39bf9def492
production <production>
2009-05-06 23:28:55 +0000

    change SUMLIB_DelSeriesSU() to char * arg

--- base/sums/libs/api/sum_rpc.h
+++ base/sums/libs/api/sum_rpc.h
@@ -516,7 +516,7 @@ int SUMLIB_TapeFilenumber(char *tapeid);
 int SUMLIB_TapeFindGroup(int group, double bytes, TAPE *tape);
 int SUMLIB_PavailGet(double bytes, int pds_set, uint64_t uid, uint64_t sunum, KEY **results);
 int SUMLIB_PavailUpdate(char *name, double bytes);
-int SUMLIB_DelSeriesSU(KEY *params); 
+int SUMLIB_DelSeriesSU(char *file); 
 int SUMLIB_InfoGet(uint64_t sunum , KEY **results);
 
 
