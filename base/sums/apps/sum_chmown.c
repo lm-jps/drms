209743f09637934245f1f0b587f6eeee14e11a0c
rick <rick>
2009-06-04 22:22:56 +0000

    added suport for site localization via serverdefs and localization

--- base/sums/apps/sum_chmown.c
+++ base/sums/apps/sum_chmown.c
@@ -5,46 +5,23 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <pwd.h>
+/*  #include <grp.h>  */
 #include <dirent.h>
 
-#define UID 388
-#define GID 320
-//#define PATTERN "^/SUM[0-9]+/D[0-9]+$"
-#define PATTERN "^/SUM[0-9]+/D[0-9]+"
+#include "serverdefs.h"
 
-void recursive_chmown(const char *dir);
-void die(const char *fmt, ...);
+#define PATTERN "^/SUM[0-9]*/D[0-9]+$"
 
-int main(int argc, char *argv[])
-{
-    regex_t reg;
-    struct stat fs;
-
-    if (argc != 2)
-	die("Usage: %s <SUDIR>\n", argv[0]);
-
-    if (regcomp(&reg, PATTERN, REG_EXTENDED | REG_NOSUB))
-	die("%s: bad regex %s\n", argv[0], PATTERN);
-    if (regexec(&reg, argv[1], 0, 0, 0))
-	die("%s: non SUDIR %s ignored\n", argv[0], argv[1]);
-
-    if (lstat(argv[1], &fs) < 0)
-	die("%s: can't lstat directory %s\n", argv[0], argv[1]);
-    if (!S_ISDIR(fs.st_mode))
-	die("%s: %s is not a directory\n", argv[0], argv[1]);
-
-    if (chown(argv[1], UID, GID) < 0)
-	die("%s: can't chown %s\n", argv[0], argv[1]);
-    if (chmod(argv[1], 0755) < 0)
-	die("%s: can't chmod %s\n", argv[0], argv[1]);
-    recursive_chmown(argv[1]);
-
-    return 0;
+void die(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    exit(1);
 }
 
-////////////////////////////////////////////////////////////////////////////////
-void recursive_chmown(const char *dir)
-{
+void recursive_chmown(const char *dir, uid_t owner, gid_t group) {
     DIR *d;
     struct dirent *entry;
     struct stat fs;
@@ -66,13 +43,13 @@ void recursive_chmown(const char *dir)
 	    die("recursive_chmown(): can't lstat dirent %s\n", pname);
 
 	if (S_ISDIR(fs.st_mode)) {
-	    if (chown(pname, UID, GID) < 0)
+	    if (chown(pname, owner, group) < 0)
 		die("recursive_chmown(): can't chown %s\n", pname);
 	    if (chmod(pname, 0755) < 0)
 		die("recursive_chmown(): can't chmod %s\n", pname);
 	    recursive_chmown(pname);
 	} else if (S_ISREG(fs.st_mode)) {
-	    if (chown(pname, UID, GID) < 0)
+	    if (chown(pname, owner, group) < 0)
 		die("recursive_chmown(): can't chown %s\n", pname);
 	    if (chmod(pname, 0644) < 0)
 		die("recursive_chmown(): can't chmod %s\n", pname);
@@ -82,12 +59,33 @@ void recursive_chmown(const char *dir)
     closedir(d);
 }
 
-////////////////////////////////////////////////////////////////////////////////
-void die(const char *fmt, ...)
-{
-    va_list ap;
-    va_start(ap, fmt);
-    vfprintf(stderr, fmt, ap);
-    va_end(ap);
-    exit(1);
+int main(int argc, char *argv[]) {
+    regex_t reg;
+    struct stat fs;
+    struct passwd *pwd = getpwnam (SUMS_MANAGER);
+    uid_t sumsowner = pwd->pw_uid;
+/*
+    struct group *grp = getgrnam (SUMS_GROUP);
+    gid_t sumsgroup = grp->gr_gid;
+*/
+    if (argc != 2)
+	die("Usage: %s <SUDIR>\n", argv[0]);
+
+    if (regcomp(&reg, PATTERN, REG_EXTENDED | REG_NOSUB))
+	die("%s: bad regex %s\n", argv[0], PATTERN);
+    if (regexec(&reg, argv[1], 0, 0, 0))
+	die("%s: non SUDIR %s ignored\n", argv[0], argv[1]);
+
+    if (lstat(argv[1], &fs) < 0)
+	die("%s: can't lstat directory %s\n", argv[0], argv[1]);
+    if (!S_ISDIR(fs.st_mode))
+	die("%s: %s is not a directory\n", argv[0], argv[1]);
+
+    if (chown(argv[1], sumsowner, -1) < 0)
+	die("%s: can't chown %s\n", argv[0], argv[1]);
+    if (chmod(argv[1], 0755) < 0)
+	die("%s: can't chmod %s\n", argv[0], argv[1]);
+    recursive_chmown(argv[1], sumsowner, -1);
+
+    return 0;
 }
