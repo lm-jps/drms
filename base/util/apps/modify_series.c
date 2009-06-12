6ffe527df8020c6c0ddc48b1535db363ead171b7
jennifer <jennifer>
2009-06-12 22:18:46 +0000

    Changed to use a larger buffer size in field_list, also removed any discussion of indexes in dropping table.

--- base/util/apps/modify_series.c
+++ base/util/apps/modify_series.c
@@ -32,6 +32,7 @@ create_series delete_series describe_series show_info
 */
 #include "drms.h"
 #include "jsoc_main.h"
+#include "util.h"
 
 /* Command line parameter values. */
 ModuleArgs_t module_args[] = { 
@@ -47,7 +48,8 @@ int DoIt(void) {
   int status = DRMS_NO_ERROR;
   int archive;
   char *series, *series_short, *namespace;
-  char stmt[bufsz];
+  char stmt [DRMS_MAXQUERYLEN];
+  //char stmt[2048];
   DB_Text_Result_t *qres = NULL;
   char *filename;
   FILE *fp;
@@ -56,7 +58,7 @@ int DoIt(void) {
   DRMS_Record_t *template;
 
   /* Parse command line parameters. */
-  if (cmdparams_numargs (&cmdparams) < 3) goto usage;
+  if (cmdparams_numargs (&cmdparams) < 2) goto usage;
 
   /* Get series name. */
   series = cmdparams_get_str(&cmdparams, "series", NULL);
@@ -132,16 +134,8 @@ int DoIt(void) {
     status = parse_keywords(buf, template, NULL);
     free(buf);
 
-    // rename series table and sequence
-    snprintf(stmt,  bufsz, "drop index %s_prime_idx; alter table %s rename to %s_tmp; alter table %s_seq rename to %s_tmp_seq", series, series, series_short, series, series_short);
-    if(drms_dms(drms_env->session, NULL, stmt)) {
-      fprintf(stderr, "Failed: %s\n", stmt);
-      status = 1;
-      goto bailout;
-    }
-
-    // delete current series without dropping the series table and
-    // sequence. take care not to free the template.
+    // Delete current series metadata without dropping the series table and
+    // sequence. Take care not to free the template. Use cascade=0 in drms_delete_series to avoid dropping table.
     void (*pFn)(const void *);
     pFn = (drms_env->series_cache).deep_free; /* temp var to hold fn. */
     (drms_env->series_cache).deep_free = NULL;
@@ -150,13 +144,25 @@ int DoIt(void) {
       status = 1;
       goto bailout;
     }
+
+    // rename series table and sequence
+    snprintf(stmt,  bufsz, "alter table %s rename to %s_tmp; alter table %s_seq rename to %s_tmp_seq", series, series_short, series, series_short);
+    //if ((qres = drms_query_txt(drms_env->session, stmt)) && qres->num_rows>0) {
+    if(drms_dms(drms_env->session, NULL, stmt)) {
+       fprintf(stderr, "Failed: %s\n", stmt);
+       status = 1;
+        goto bailout;
+    }
+
+
     // restore deep_free function
     (drms_env->series_cache).deep_free = pFn;
 
     // retain the spot in the series_cache. this is a hack to undo
     // hcon_remove()
+    // Enclosed the hcon_remove() in an 'if (cascade)' stmt, so can remove this hack.
     hcon_allocslot_lower(&drms_env->series_cache, series);
-    //    drms_print_record(template);
+        drms_print_record(template);
 
     // create a new series
     if (drms_insert_series(drms_env->session, 0, template, 0)) {
@@ -174,13 +180,29 @@ int DoIt(void) {
     }
 
     // move back contents of series
-    snprintf(stmt, bufsz, "insert into %s (%s) select %s from %s_tmp", series, field_list, field_list, series);
-    if(drms_dms(drms_env->session, NULL, stmt)) {
-      fprintf(stderr, "Failed: %s\n", stmt);
+    //snprintf(stmt, bufsz, "insert into %s (%s) select %s from %s_tmp", series, field_list, field_list, series);
+    //buffer size could be huge, above statment doesn't have enough space.  Need to build with base_strcatalloc instead.
+    char *stmt2 = malloc(DRMS_MAXQUERYLEN);
+    size_t bufsize = DRMS_MAXQUERYLEN;
+    memset (stmt2, 0, bufsize);
+     base_strcatalloc(stmt2, "insert into ", &bufsize);
+     base_strcatalloc(stmt2, series, &bufsize);
+     base_strcatalloc(stmt2, " (", &bufsize);
+     base_strcatalloc(stmt2, field_list, &bufsize);
+     base_strcatalloc(stmt2, ") ", &bufsize);
+     base_strcatalloc(stmt2, "select ", &bufsize);
+     base_strcatalloc(stmt2, field_list, &bufsize);
+     base_strcatalloc(stmt2, " from ", &bufsize);
+     base_strcatalloc(stmt2, series, &bufsize);
+     base_strcatalloc(stmt2, "_tmp", &bufsize);
+
+    if(drms_dms(drms_env->session, NULL, stmt2)) {
+      fprintf(stderr, "Failed: %s\n", stmt2);
       status = 1;
       goto bailout;
     }
     free(field_list);
+    free(stmt2);
 
     srand((unsigned int) time(NULL)+getpid());
     snprintf(stmt, bufsz, "create table backup.%s_%s_%d as select * from %s_tmp;drop table %s_tmp", namespace, series_short, rand(), series, series);
