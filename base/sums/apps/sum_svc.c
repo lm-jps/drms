f4c3651af7cd0fc8f10fcbf098b998d10b1ee8ff
production <production>
2009-06-16 19:24:40 +0000

    move sum_svc from d02 to j1

--- base/sums/apps/sum_svc.c
+++ base/sums/apps/sum_svc.c
@@ -50,6 +50,7 @@ CLIENT *current_client, *clnttape;
 SVCXPRT *glb_transp;
 char *dbname;
 char thishost[MAX_STR];
+char hostn[MAX_STR];
 char logname[MAX_STR];
 char datestr[32];
 char timetag[32];
@@ -207,7 +208,7 @@ void setup()
 {
   FILE *fplog;
   int tpid, i;
-  //char *cptr;
+  char *cptr;
   char lfile[MAX_STR], acmd[MAX_STR], line[MAX_STR];
 
   //when change name of dcs2 to dcs1 we found out you have to use localhost
@@ -215,13 +216,16 @@ void setup()
   //cptr = index(thishost, '.');       /* must be short form */
   //if(cptr) *cptr = (char)NULL;
   sprintf(thishost, "localhost");
+  gethostname(hostn, 80);
+  cptr = index(hostn, '.');     // must be short form
+  if(cptr) *cptr = (char)NULL;
 
   thispid = getpid();
   sprintf(logname, "%s/sum_svc_%s.log", SUMLOG_BASEDIR, gettimetag());
   open_log(logname);
   printk_set(write_log, write_log);
-  write_log("\n## %s sum_svc on %s for pid = %d ##\n", 
-		datestring(), thishost, thispid);
+  write_log("\n## %s sum_svc on %s (%s) for pid = %d ##\n", 
+		datestring(), thishost, hostn, thispid);
   write_log("Database to connect to is %s\n", dbname);
   //if (signal(SIGINT, SIG_IGN) != SIG_IGN)
       signal(SIGINT, sighandler);
@@ -260,17 +264,12 @@ int main(int argc, char *argv[])
   register SVCXPRT *transp;
   int i;
   pid_t pid;
-  char dsvcname[80];
+  char dsvcname[80], cmd[128];
   char *args[5], pgport[32];
-  char hostn[80];
-  char *cptr;
 
   get_cmd(argc, argv);
   printf("\nPlease wait for sum_svc and tape inventory to initialize...\n");
   setup();
-  gethostname(hostn, 80);
-  cptr = index(hostn, '.');     // must be short form
-  if(cptr) *cptr = (char)NULL;
 
         /* register for client API routines to talk to us */
 	(void) pmap_unset(SUMPROG, SUMVERS);
@@ -286,92 +285,9 @@ int main(int argc, char *argv[])
 	}
 
   if(strcmp(hostn, "dcs0") && strcmp(hostn, "dcs1") && strcmp(hostn, "dcs2") && strcmp(hostn, "dcs3") && strcmp(hostn, "n02")) {  //!!TEMP n02 for testing
-    sprintf(pgport, SUMPGPORT);
-    setenv("PGPORT", pgport, 1); //need to connect to new jsoc_sums db
+      sprintf(pgport, SUMPGPORT);
+      setenv("PGPORT", pgport, 1); //need to connect to new jsoc_sums db
   }
-
-#ifndef __LOCALIZED_DEFS__
-/* !!TEMP don't fork on lws or flap, etc. which have test SUMS */
-if(strcmp(hostn, "lws") && strcmp(hostn, "flap") && strcmp(hostn, "d00") && strcmp(hostn, "n02")) {
-  if((pid = fork()) < 0) {
-    write_log("***Can't fork(). errno=%d\n", errno);
-    exit(1);
-  }
-  else if(pid == 0) {                   /* this is the beloved child */
-    write_log("execvp of tape_svc\n");
-    args[0] = "tape_svc";
-    if(tapeoffline) { 		/* overrides any sim flg */
-      args[1] = "-o";
-      args[2] = dbname;
-      args[3] = timetag;
-      args[4] = NULL;
-    }
-    else if(sim) { 
-      args[1] = "-s";
-      args[2] = dbname;
-      args[3] = timetag;
-      args[4] = NULL;
-    }
-    else {
-      args[1] = dbname;
-      args[2] = timetag;
-      args[3] = NULL;
-    }
-    if(execvp(args[0], args) < 0) {
-      write_log("***Can't execvp() tape_svc. errno=%d\n", errno);
-      exit(1);
-    }
-  }
-  sleep(1);				/* let tape_svc start */
-  for(i=0; i < MAX_DRIVES; i++) { 	/* start all the driven_svc */
-    if((pid = fork()) < 0) {
-      write_log("***Can't fork(). errno=%d\n", errno);
-      exit(1);
-    }
-    else if(pid == 0) {                   /* this is the beloved child */
-      sprintf(dsvcname, "drive%d_svc", i);
-      write_log("execvp of %s\n", dsvcname);
-      args[0] = dsvcname;
-      if(tapeoffline) {                 /* overrides any sim flg */
-	 args[1] = "-o";
-	 args[2] = dbname;
-	 args[3] = timetag;
-	 args[4] = NULL;
-      }
-      else if(sim) {
-        args[1] = "-s";
-        args[2] = dbname;
-        args[3] = timetag;
-        args[4] = NULL;
-      }
-      else {
-        args[1] = dbname;
-        args[2] = timetag;
-        args[3] = NULL;
-      }
-      if(execvp(args[0], args) < 0) {
-        write_log("***Can't execvp() %s. errno=%d\n", dsvcname, errno);
-        exit(1);
-      }
-    }
-  }
-  if((pid = fork()) < 0) {
-    write_log("***Can't fork(). errno=%d\n", errno);
-    exit(1);
-  }
-  else if(pid == 0) {                   /* this is the beloved child */
-    write_log("execvp of robot0_svc\n");
-    args[0] = "robot0_svc";
-    args[1] = dbname;
-    args[2] = timetag;
-    args[3] = NULL;
-    if(execvp(args[0], args) < 0) {
-      write_log("***Can't execvp() robot0_svc. errno=%d\n", errno);
-      exit(1);
-    }
-  }
-}				/* !!end of TMP for lws only */
-#endif
 
   if((pid = fork()) < 0) {
     write_log("***Can't fork(). errno=%d\n", errno);
@@ -392,10 +308,16 @@ if(strcmp(hostn, "lws") && strcmp(hostn, "flap") && strcmp(hostn, "d00") && strc
 #ifndef __LOCALIZED_DEFS__
 if(strcmp(hostn, "lws") && strcmp(hostn, "n00") && strcmp(hostn, "d00") && strcmp(hostn, "n02")) { 
   /* Create client handle used for calling the tape_svc */
-  sleep(3);			/* give time to start */
-  clnttape = clnt_create(thishost, TAPEPROG, TAPEVERS, "tcp");
+  printf("\nsum_svc waiting for tape servers to start (approx 10sec)...\n");
+  sleep(10);			/* give time to start */
+  //if running on j1, then the tape_svc is on TAPEHOST, else the localhost
+  if(strcmp(hostn, "j1")) 
+    clnttape = clnt_create(thishost, TAPEPROG, TAPEVERS, "tcp");
+  else {
+    clnttape = clnt_create(TAPEHOST, TAPEPROG, TAPEVERS, "tcp");
+  }
   if(!clnttape) {       /* server not there */
-    clnt_pcreateerror("Can't get client handle to tape_svc");
+    clnt_pcreateerror("Can't get client handle to tape_svc (xsum_svc)");
     write_log("tape_svc not there on %s\n", thishost);
     exit(1);
   }
