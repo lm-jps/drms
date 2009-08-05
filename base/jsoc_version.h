4c8186952641d31f72cd2a4c8c650d412a99eab8
arta <arta>
2009-08-05 23:54:22 +0000

    back to development version number

--- base/jsoc_version.h
+++ base/jsoc_version.h
@@ -13,8 +13,8 @@
 
 #define JSOC_VERSION_INCL 1
 
-#define jsoc_version "V5R3"
-#define jsoc_vers_num (503)
+#define jsoc_version "V5R3X"
+#define jsoc_vers_num (-503)
 
 
 static inline const char *jsoc_getversion(char *verstr, int size, int *isdev)
