bdf161dd7dd0167d691ed14a160312991d8cabc0
arta <arta>
2009-10-08 06:03:56 +0000

    Add parameter that accepts database name; pass that name to the psql -f calls.

--- base/drms/scripts/createpgfuncs.pl
+++ base/drms/scripts/createpgfuncs.pl
@@ -1,5 +1,16 @@
 #!/usr/bin/perl -w
 
+# read in the dbname
+my($dbname);
+
+if (scalar(@ARGV) != 1)
+{
+    print STDERR "A single argument, the name of the DRMS database, is expected.\n";
+    exit(1);
+}
+
+$dbname = $ARGV[0];
+
 if (-d "scripts")
 {
     chdir("scripts");
@@ -11,17 +22,17 @@ if (-d "scripts")
 # These might error out if the type doesn't already exist, but it is 
 # safe to ignore the error message (the redirection redirects stderr
 # to stdout, which then gets dropped).
-`psql -c "DROP TYPE drmskw CASCADE" 2>&1`;
-`psql -c "DROP TYPE rep_item CASCADE" 2>&1`;
-`psql -c "DROP TYPE drmsseries CASCADE" 2>&1`;
-`psql -c "DROP TYPE drmssession CASCADE" 2>&1`;
+`psql -c "DROP TYPE drmskw CASCADE" $dbname 2>&1`;
+`psql -c "DROP TYPE rep_item CASCADE" $dbname 2>&1`;
+`psql -c "DROP TYPE drmsseries CASCADE" $dbname 2>&1`;
+`psql -c "DROP TYPE drmssession CASCADE" $dbname 2>&1`;
 
 # Now create the types/functions
-`psql -f create_type_drmskw.sql`;
-`psql -f drms_keyword.sql`;
-`psql -f create_type_rep_item.sql`;
-`psql -f drms_replicated.sql`;
-`psql -f create_type_drmsseries.sql`;
-`psql -f drms_series.sql`;
-`psql -f create_type_drmssession.sql`;
-`psql -f drms_session.sql`;
+`psql -f create_type_drmskw.sql $dbname`;
+`psql -f drms_keyword.sql $dbname`;
+`psql -f create_type_rep_item.sql $dbname`;
+`psql -f drms_replicated.sql $dbname`;
+`psql -f create_type_drmsseries.sql $dbname`;
+`psql -f drms_series.sql $dbname`;
+`psql -f create_type_drmssession.sql $dbname`;
+`psql -f drms_session.sql $dbname`;
