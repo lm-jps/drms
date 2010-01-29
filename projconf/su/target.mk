7e0ba6c002a046f2fc5fecdfd5573afd767dde4b
tplarson <tplarson>
2010-01-29 22:41:20 +0000

    added globalhs/apps/src

--- projconf/su/target.mk
+++ projconf/su/target.mk
@@ -16,4 +16,5 @@ $(PROJOBJDIR):
 	+@[ -d $@/export/apps ] || mkdir -p $@/export/apps
 	+@[ -d $@/export/libs/util ] || mkdir -p $@/export/libs/util
 	+@[ -d $@/globalhs/apps/ ] || mkdir -p $@/globalhs/apps
+	+@[ -d $@/globalhs/apps/src/ ] || mkdir -p $@/globalhs/apps/src
 	+@[ -d $@/rings/apps/ ] || mkdir -p $@/rings/apps
