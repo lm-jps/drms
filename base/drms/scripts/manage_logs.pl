ff73932d8dbbfda5c166a6cfb4040b7f1ee87ab4
arta <arta>
2010-03-16 21:21:29 +0000

    If no counter file is provided, then simply delete old files - ones that have been on the server for greater than 10 days.

--- base/drms/scripts/manage_logs.pl
+++ base/drms/scripts/manage_logs.pl
@@ -1,8 +1,16 @@
 #!/usr/bin/perl -w
 
 # Usage:
-#    manage_logs.pl -s <conf file> -c <counter file name> [-t <tar cmd> -g <gzip cmd>] [-l <log file>]
-#    manage_logs.pl -s subscription_manager.cfg -c slony_counter.txt -t /bin/tar -z /bin/gzip -l tmp.txt
+#    manage_logs.pl -s <conf file> -c <counter file name> -p <parse lock file> -a <tar lock file> [-t <tar cmd> -z <gzip cmd>] [-l <log file>]
+#    manage_logs.pl -s subscription_manager.cfg -c slony_counter.txt -p /usr/local/pgsql/slon_logs/parselock.txt -t /bin/tar -z /bin/gzip -l tmp.txt
+#    manage_logs.pl -s /b/devtest/etc/subscription_manager.cfg -p /usr/local/pgsql/slon_logs/parselock.txt -t /bin/tar -z /bin/gzip -l /usr/local/pgsql/log/manage_logs.log
+#    manage_logs.pl -s /solarport/pgsql/slon_logs/site_logs_test/subscription_manager.cfg -p /usr/local/pgsql/slon_logs/parselock.txt -t /bin/tar -z /bin/gzip -l tmp.txt
+#     ~arta/JSOC/base/drms/scripts/manage_logs.pl -s /solarport/pgsql/slon_logs/site_logs_test/subscription_manager.cfg -p /usr/local/pgsql/slon_logs/parselock.txt -t /bin/tar -z /bin/gzip -l tmp.txt
+
+# The lock file synchronizes access to site-specific logs. parse_slony_logs produces the
+# site-specific logs, and this script consumes them. To avoid a race condition, 
+# parse_slony_logs creates a file lock. This script must check the file lock, and 
+# proceed only if the file lock does not exist. 
 
 # Runs on the slony server (at Stanford, this is webdb)
 
@@ -10,8 +18,11 @@ use IO::Dir;
 use FileHandle;
 use File::Copy;
 use Fcntl ':flock';
+use Time::HiRes qw(gettimeofday);
 
 use constant kTarChunk => 64;
+# Number of days to keep tar files on server
+use constant kExp => 10;
 
 my($arg);
 my($siteDir);   # dir on server containing site-specific slony log files
@@ -21,8 +32,11 @@ my($dotar);     # if set, the tar up files
 my($tarbin);    # path to tar
 my($zipbin);    # path to zip
 my($logfile);   # file to log output to
+my($exp);
 my($line);
-
+my($parselock);
+my($plockfh);
+my($natt);
 
 ## only one version of this program running
 unless (flock(DATA, LOCK_EX|LOCK_NB)) 
@@ -42,6 +56,10 @@ while ($arg = shift(@ARGV))
    {
       $cntrFile = shift(@ARGV);
    }
+   elsif ($arg eq "-p")
+   {
+      $parselock = shift(@ARGV);
+   }
    elsif (($pos = index($arg, "-t", 0)) == 0)
    {
       $dotar = 1;
@@ -57,9 +75,44 @@ while ($arg = shift(@ARGV))
    }
 }
 
-if (!defined($cnfPath) || !defined($cntrFile) || (defined($tarbin) && !defined($zipbin)))
+if (!defined($cnfPath) || (defined($tarbin) && !defined($zipbin)))
 {
    die "Invalid arguments.\n";
+}
+
+if (!defined($cntrFile))
+{
+   # No file that specifies a log number of the last log fetched by get_slony_logs.pl.
+   # Instead, remove all files older than kExp days.
+   $exp = kExp * 86400;
+}
+
+# Parse lock
+$plockfh = FileHandle->new(">$parselock");
+
+$natt = 0;
+while (1)
+{
+   if (flock($plockfh, LOCK_EX|LOCK_NB)) 
+   {
+      print "Created parse-lock file '$parselock'.\n";
+      last;
+   }
+   else
+   {
+      if ($natt < 10)
+      {
+         print "parse_slony_logs is currently modifying files - waiting for completion.\n";
+         sleep 1;
+      }
+      else
+      {
+         print "couldn't obtain parse lock; bailing.\n";
+         exit(1);
+      }
+   }
+
+   $natt++;
 }
 
 # Ingest the global configuration file.
@@ -120,26 +173,30 @@ while (defined($asite = shift(@subdirs)))
 
    # Get counter file
    $cntrVal = undef;
-   $cntrPath = "${asite}/${cntrFile}";
 
-   if (-f $cntrPath)
+   if (defined($cntrFile))
    {
-      # Get counter value
-      open(CTRFILE, "<$cntrPath") || die "Unable to read $cntrPath.\n";
-      if (defined($line = <CTRFILE>))
-      {
-         chomp($line);
-         $cntrVal = $line;
-      }
+      $cntrPath = "${asite}/${cntrFile}";
 
-      close(CTRFILE);
+      if (-f $cntrPath)
+      {
+         # Get counter value
+         open(CTRFILE, "<$cntrPath") || die "Unable to read $cntrPath.\n";
+         if (defined($line = <CTRFILE>))
+         {
+            chomp($line);
+            $cntrVal = $line;
+         }
+
+         close(CTRFILE);
+      }
    }
 
    tie(my(%logfiles), "IO::Dir", $asite);
    @lfiles = keys(%logfiles);
 
    # Get sorted (by counter number) lists of parsed slony logs and tar files
-   @sqlfiles = sort({($a =~ /^slony\S*_log\S*_(\d+)\.sql/)[0] <=> ($b =~ /^slony\S*_log\S*_(\d+)\.sql/)[0]} map({$_ =~ /^(slony\S*_log\S*_\d+\.sql)/ ? $1 : ()} @lfiles));
+   @sqlfiles = sort({($a =~ /^slony\S*_log\S*_(\d+)\.sql$/)[0] <=> ($b =~ /^slony\S*_log\S*_(\d+)\.sql$/)[0]} map({$_ =~ /^(slony\S*_log\S*_\d+\.sql)$/ ? $1 : ()} @lfiles));
    @tarfiles = sort({($a =~/(\d+)-\d+/)[0] <=> ($b =~ /(\d+)-\d+/)[0]} map({$_ =~ /^(slony\S*_logs\S*_\d+-\d+\.tar.*)/ ? $1 : ()} @lfiles));
 
    # Untie before trying to delete files
@@ -164,6 +221,11 @@ while (defined($asite = shift(@subdirs)))
    my($sfilestart);
    my($sfileend);
    my(@totar);
+   my($ftstamp);
+   my($today);
+   my(@statret);
+   
+   $today = gettimeofday;
 
    #sql files
    $isfile = 0;
@@ -173,12 +235,17 @@ while (defined($asite = shift(@subdirs)))
       {
          $ccounter = $1;
 
-         if ((defined($lasttarred) && $ccounter <= $lasttarred) || 
-             (defined($cntrVal) && $ccounter <= $cntrVal))
+         $fullpath = "${asite}/${item}";
+         @statret = stat($fullpath);
+         $ftstamp = $statret[10];
+
+         # If we know for sure that a file with a counter value has been ingested already, 
+         # or the file has been sitting on the server a long time, delete it.
+         if ((defined($cntrVal) && $ccounter <= $cntrVal) ||
+             (defined($exp) && $ftstamp + $exp <= $today))
          {
-            # Already tarred or ingested, okay to remove
-            $fullpath = "${asite}/${item}";
-            LogWrite($logfh, "Deleting sql file previously ingested: $fullpath.\n", 0);
+            # Already tarred or ingested, or expired, okay to remove
+            LogWrite($logfh, "Deleting sql file previously ingested or expired: $fullpath.\n", 0);
             unlink($fullpath);
          }
          elsif ($dotar)
@@ -199,7 +266,7 @@ while (defined($asite = shift(@subdirs)))
    }
 
    # tar files
-   if (defined($cntrVal))
+   if (defined($cntrVal) || defined($exp))
    {
       foreach $item (@tarfiles)
       {
@@ -207,11 +274,13 @@ while (defined($asite = shift(@subdirs)))
          {
             $ccounter = $1;
 
-            if ($ccounter <= $cntrVal)
+            $fullpath = "${asite}/${item}";
+            $ftstamp = (stat($fullpath))[10];
+
+            if ((defined($cntrVal) && $ccounter <= $cntrVal) || (defined($exp) && $ftstamp + $exp <= $today))
             {
-               # Already ingested, okay to remove
-               $fullpath = "${asite}/${item}";
-               LogWrite($logfh, "Deleting tar file previously ingested: $fullpath.\n", 0);
+               # Already ingested or expired, okay to remove
+               LogWrite($logfh, "Deleting tar file previously ingested or expired: $fullpath.\n", 0);
                unlink($fullpath);
             }
          }
@@ -301,6 +370,9 @@ while (defined($asite = shift(@subdirs)))
 } # end site loop
 
 untie(%sites);
+
+flock($plockfh, LOCK_UN);
+$plockfh->close;
 
 if (defined($logfh))
 {
