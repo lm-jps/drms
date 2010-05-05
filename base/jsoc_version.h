247969adc2fc13f47c3a6ba673eabc4ff8d8fd77
arta <arta>
2010-05-05 01:03:53 +0000

    Switch to the developer version of the jsoc version

--- base/jsoc_version.h
+++ base/jsoc_version.h
@@ -13,8 +13,8 @@
 
 #define JSOC_VERSION_INCL 1
 
-#define jsoc_version "V5R9"
-#define jsoc_vers_num (509)
+#define jsoc_version "V5R9X"
+#define jsoc_vers_num (-509)
 
 
 static inline const char *jsoc_getversion(char *verstr, int size, int *isdev)
