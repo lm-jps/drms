34dfc7edd37602b76bde7860bcf9b902104d41e0
arta <arta>
2011-01-19 20:40:37 +0000

    Make the compiler autoconfiguration configurable - it can be turned off so that only manual configuration occurs.

--- moreconfigure.pl
+++ moreconfigure.pl
@@ -15,115 +15,159 @@ use constant GFORTMINOR => 2;
 
 my($arg);
 my($pos);
+my($outdir);
 my($outfile);
+my($customdefsfile);
 my($major);
 my($minor);
 my($hasicc);
 my($hasifort);
 my($hasgcc);
 my($hasgfort);
+my($skipautocomp);
+my($line);
 
 while ($arg = shift(@ARGV))
 {
-    if (($pos = index($arg, "-f", 0)) == 0)
+    if (($pos = index($arg, "-d", 0)) == 0)
+    {
+	$outdir = substr($arg, 2);
+    }
+    elsif (($pos = index($arg, "-f", 0)) == 0)
     {
 	$outfile = substr($arg, 2);
     }
+    elsif (($pos = index($arg, "-c", 0)) == 0)
+    {
+	$customdefsfile = substr($arg, 2);
+    }
 }
+
+$outfile = "$outdir/$outfile";
 
 $hasicc = 0;
 $hasifort = 0;
 $hasgcc = 0;
 $hasgfort = 0;
+$skipautocomp = 0;
 
-if (defined($outfile))
+if (defined($outdir) && defined($outfile))
 {
-    # Try icc
-    $ans = `icc -V 2>&1`;      
+   # Check to see if user does not want to auto-configure compiler make variables
+   # Read the file containing the defs (right now, customizeddefs.h for Stanford, 
+   # localization.h for non-Stanford).
+   if (defined($customdefsfile) && -e $customdefsfile)
+   {
+      if (open(DEFSFILE, "<$customdefsfile"))
+      {
+         my($doautocomp) = 0;
 
-    if (defined($ans) && $ans =~ /Version\s+(\d+)\.(\d+)/)
-    {
-        $major = $1;
-        $minor = $2;
+         while (defined($line = <DEFSFILE>))
+         {
+            if ($line =~ /AUTOSELCOMP\s+(.+)/)
+            {
+               $doautocomp = $1;
+               $skipautocomp = !$doautocomp;
+               last;
+            }
+         }
 
-        if (IsVersion($major, $minor, ICCMAJOR, ICCMINOR))
-        {
+         close(DEFSFILE);
+      }
+   }
+
+   if (!$skipautocomp)
+   {
+      # Try icc
+      $ans = `icc -V 2>&1`;      
+
+      if (defined($ans) && $ans =~ /Version\s+(\d+)\.(\d+)/)
+      {
+         $major = $1;
+         $minor = $2;
+
+         if (IsVersion($major, $minor, ICCMAJOR, ICCMINOR))
+         {
             $hasicc = 1;
-        }
-    }
+         }
+      }
 
-    # Try gcc
-    if (!$hasicc)
-    {
-        $ans = `gcc -v 2>&1`;
-        if (defined($ans) && $ans =~ /gcc\s+version\s+(\d+)\.(\d+)/)
-        {
+      # Try gcc
+      if (!$hasicc)
+      {
+         $ans = `gcc -v 2>&1`;
+         if (defined($ans) && $ans =~ /gcc\s+version\s+(\d+)\.(\d+)/)
+         {
             $major = $1;
             $minor = $2;
 
             if (IsVersion($major, $minor, GCCMAJOR, GCCMINOR))
             {
-                $hasgcc = 1;
+               $hasgcc = 1;
             }
-        }
-    }
+         }
+      }
 
-    # Try ifort
-    $ans = `ifort -V 2>&1`;
-    if (defined($ans) && $ans =~ /Version\s+(\d+)\.(\d+)/)
-    {
-        $major = $1;
-        $minor = $2;
+      # Try ifort
+      $ans = `ifort -V 2>&1`;
+      if (defined($ans) && $ans =~ /Version\s+(\d+)\.(\d+)/)
+      {
+         $major = $1;
+         $minor = $2;
 
-        if (IsVersion($major, $minor, IFORTMAJOR, IFORTMINOR))
-        {
+         if (IsVersion($major, $minor, IFORTMAJOR, IFORTMINOR))
+         {
             $hasifort = 1;
-        }
-    }
+         }
+      }
 
-    # Try gfortran
-    if (!$hasifort)
-    {
-        $ans = `gfortran -v 2>&1`;
-        if (defined($ans) && $ans =~ /gcc\s+version\s+(\d+)\.(\d+)/)
-        {
+      # Try gfortran
+      if (!$hasifort)
+      {
+         $ans = `gfortran -v 2>&1`;
+         if (defined($ans) && $ans =~ /gcc\s+version\s+(\d+)\.(\d+)/)
+         {
             $major = $1;
             $minor = $2;
 
             if (IsVersion($major, $minor, GFORTMAJOR, GFORTMINOR))
             {
-                $hasgfort = 1;
+               $hasgfort = 1;
             }
-        }
-    }
+         }
+      }
+   }
 
     open(OUTFILE, ">>$outfile") || die "Couldn't open file $outfile for writing.\n";
 
     # Error messages
-    if (!$hasicc && !$hasgcc)
+    if (!$skipautocomp)
     {
-        print "Fatal error: Acceptable C compiler not found! You will be unable to build the DRMS library.\n";
-    }
-    elsif ($hasicc)
-    {
-        print OUTFILE "COMPILER = icc\n";
-    }
-    else
-    {
-        print OUTFILE "COMPILER = gcc\n";
-    }
+       if (!$hasicc && !$hasgcc)
+       {
+          print "Fatal error: Acceptable C compiler not found! You will be unable to build the DRMS library.\n";
+       }
+       elsif ($hasicc)
+       {
+          print OUTFILE "COMPILER = icc\n";
+       }
+       else
+       {
+          print OUTFILE "COMPILER = gcc\n";
+       }
 
-    if (!$hasifort && !$hasgfort)
-    {
-        print "Warning: Acceptable Fortran compiler not found! Fortran interface will not be built, and you will be unable to build Fortran modules.\n";
-    }
-    elsif ($hasifort)
-    {
-        print OUTFILE "FCOMPILER = ifort\n";
-    }
-    else
-    {
-        print OUTFILE "FCOMPILER = gfortran\n";
+       if (!$hasifort && !$hasgfort)
+       {
+          print "Warning: Acceptable Fortran compiler not found! Fortran interface will not be built, and you will be unable to build Fortran modules.\n";
+       }
+       elsif ($hasifort)
+       {
+          print OUTFILE "FCOMPILER = ifort\n";
+       }
+       else
+       {
+          print OUTFILE "FCOMPILER = gfortran\n";
+       }
     }
 
     # Print out env var override logic - otherwise we lose this logic in make_basic.mk
