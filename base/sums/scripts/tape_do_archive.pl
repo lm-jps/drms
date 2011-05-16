e5ac515ad65967b952aa7d549537757e03dc247a
production <production>
2011-05-16 22:56:45 +0000

    verified

--- base/sums/scripts/tape_do_archive.pl
+++ base/sums/scripts/tape_do_archive.pl
@@ -1,15 +1,17 @@
-eval 'exec /home/jsoc/bin/$JSOC_MACHINE/perl -d -S $0 "$@"'
+eval 'exec /home/jsoc/bin/linux_ia64/perl -S $0 "$@"'
     if 0;
 #/home/production/cvs/JSOC/base/sums/scripts/tape_do_archive.pl
 #
 #
 use DBI;
+use POSIX ":sys_wait_h";
 
-#!!!TBD
+$MAXFORKS = 4;		#max tapearcX forks (i.e. limit of drives to write)
+$CURRFORKS = 0;
+
 sub usage {
-  print "Show storage distribution for each SUM group.\n";
-  print "Usage: sumlookgroup.pl [-f] [-hdb_host] db_name (e.g. jsoc_sums)\n";
-  print "       -f = fast mode. Ignore that data read from tape is always in group 0\n";
+  print "Do pending archive by group by dataset by sunum.\n";
+  print "Usage: tape_do_archive.pl db_name (e.g. jsoc_sums)\n";
   print "       The default db_host is $HOSTDB\n";
   print "\nYou must have ENV SUMPGPORT set to the port number, e.g. 5434\n";
   exit(1);
@@ -55,10 +57,68 @@ sub commify {
                   return $_;
              }
 
-$ARCHGRPCFG = "/home/production/cvs/JSOC/base/sums/apps/data/arch_group.cfg";
+#Get signal when a child (tapearcX) terminates.
+#Start another if more manifest files to do.
+sub REAPCHILD {
+  my $pid;
+
+    $pid = waitpid(-1, WNOHANG);		#any pid, don't block
+    if($pid == -1) {
+      #ignore no child
+    } elsif (WIFEXITED($?)) {
+      print "Process $pid exited\n";
+      $CURRFORKS--;
+      if($newcmd = shift(@pendcmds)) {
+          print "REAPCHILD CURRFORKS=$CURRFORKS has newcmd = $newcmd\n";
+          if(!(&forkarc($newcmd))) {
+            print "Fork Error!! There s/b an open drive for write.\n";
+            exit(1);
+          }
+      }
+      else {
+        if($CURRFORKS == 0) {
+          print "All tapearcX done. Exiting.\n";
+          exit(0);
+        }
+      }
+    } else {
+      print "False alarm on $pid\n";
+    }
+  $SIG{CHLD} = \&REAPCHILD;		#old unreliable signals protection
+}
+
+#Fork another tapearcX with its manifest file. Return 0 if reached max forks.
+sub forkarc {
+  my($fcmd) = @_;
+  print "$fcmd\n";
+  if($CURRFORKS >= $MAXFORKS) { return 0; } #can't fork any more
+  $CURRFORKS++;
+    if($fpid = fork) {
+      #This is the parent. The child's pid is in $fpid
+      print stdout "child pid is $fpid. CURRFORKS = $CURRFORKS\n";
+      return 1;
+      #wait;
+    } elsif (defined $fpid) {     # $fpid is zero here if defined
+      exec $fcmd;                 # run taprarcX
+      exit;                       # never fall through
+    } else {
+      #fork error
+      print "!!! Can't fork: $!\n";
+      return 0;
+    }
+}
+
+  $host = `hostname -s`;
+  chomp($host);
+  if($host ne "d02") {
+    print "Error: This must be run on d02\n";
+    exit;
+  }
+
+$SIG{CHLD} = \&REAPCHILD;
+#$ARCHGRPCFG = "/home/production/cvs/JSOC/base/sums/apps/data/arch_group.cfg";
 $ARCHPROBE = "/home/production/cvs/JSOC/proj/util/scripts/archprobe.pl";
-#$ARCHFILEDIR = "/usr/local/logs/tapearc";
-$ARCHFILEDIR = "/tmp"; #!!TEMP
+$ARCHFILEDIR = "/usr/local/logs/manifest"; #!!TEMP
 $HOSTDB = "hmidb";      #host where DB runs
 $FAST = 0;
 while ($ARGV[0] =~ /^-/) {
@@ -74,11 +134,19 @@ while ($ARGV[0] =~ /^-/) {
 if($#ARGV != 0) {
   &usage;
 }
-$DB = $ARGV[0];		#!!!TBD fix what this arg is
+$DB = $ARGV[0];
 
 $ldate = &labeldate();
 print "tape_do_archive.pl run on $ldate\n";
-
+@ps = `ps -ef | grep tape_do_archive.pl`;
+$name = "tape_do_archive.pl $DB";
+$x = grep(/$name/, @ps);
+print "Number of tape_do_archive.pl running = $x\n";
+if($x > 1) {
+  print "A tape_do_archive.pl $DB is already running.\n";
+  print "Only one allowed. Aborting...\n";
+  exit(1);
+}
 $hostdb = $HOSTDB;      #host where Postgres runs
 $user = $ENV{'USER'};
 if(!($PGPORT = $ENV{'SUMPGPORT'})) {
@@ -86,29 +154,55 @@ if(!($PGPORT = $ENV{'SUMPGPORT'})) {
   exit;
 }
 
-      $addsec = 86400 * 100;	#sec in 100 days
-      $xeffdate = &reteffdate($addsec);
-
-open (CFG, $ARCHGRPCFG) || die "Can't Open : $ARCHGRPCFG $!\n";
-while(<CFG>) {
-  if(/^#/ || /^\n/) { #ignore any comment or blank lines
-    next;
+#First connect to database
+  $dbh = DBI->connect("dbi:Pg:dbname=$DB;host=$hostdb;port=$PGPORT", "$user", "");
+  if ( !defined $dbh ) {
+    die "Cannot do \$dbh->connect: $DBI::errstr\n";
   }
-  chomp;
-  ($group, $minwrtsz, $waitdays) = split(/\s+/);
-  push(@groups, $group);
-  push(@minwrtsz, $minwrtsz);
-  push(@waitdays, $waitdays);
-}
-push(@groups, -1);		#terminate
-print "groups=@groups\n";
-print "minwrtsz=@minwrtsz\n";
-print "waitdays=@waitdays\n";
+  $sql = "select * from sum_arch_group";
+  print "$sql\n";
+  $sth = $dbh->prepare($sql);
+  if ( !defined $sth ) {
+    print "Cannot prepare statement: $DBI::errstr\n";
+    $dbh->disconnect();
+    exit;
+  }
+  $sth->execute;
+  $i = 0;
+  while ( @row = $sth->fetchrow() ) {	#do for all groups
+    $group = shift(@row);
+    $waitdays = shift(@row);
+    $sec1970 = shift(@row);
+    $date = shift(@row);
+    $mfilename = "$ARCHFILEDIR/manifest.group$group";
+    if( -e $mfilename) {	#already exists
+      print "Manifest file $mfilename still exists.\n";
+      print "Processing for group $group may still be active. Skip...\n"; 
+      #$CURRFORKS++;	#count as active fork - No, not under our control
+      next;
+    } 
+    #determine if time to archive is past cadence
+    if($waitdays == 0) { next; }	#no archive for this group
+    $secnow = time;
+    $deltasec = $secnow - $sec1970;
+    $waitsec = $waitdays * 86400;
+    if($deltasec < $waitsec) {
+      next;
+    }
+    #Ok, archive this group. Update sec1970_start to now
+    $ldate = &labeldate();
+    $sql = "update sum_arch_group set sec1970_start=$secnow, date_arch_start='$ldate' where group_id=$group";
+      $sth2 = $dbh->prepare($sql);
+      if ( !defined $sth2 ) {
+        print "Cannot prepare statement: $DBI::errstr\n";
+        $dbh->disconnect();
+        exit;
+      }
+      $sth2->execute;
 
-$i = 0;
-while(($group = shift(@groups)) != -1) {
   $GBtotal = 0;
   print "\ngroup = $group\n";
+  @fileinfo = ();
   @dsdata = `$ARCHPROBE jsoc_sums hmidb 5434 production agg group ap ' ' $group 1`;
   print "@dsdata\n";
   while($dat = shift(@dsdata)) {
@@ -119,16 +213,12 @@ while(($group = shift(@groups)) != -1) {
     $GBtotal += $ap;
   }
   print "Total GB for group $group to archive = $GBtotal\n";
-  if($GBtotal >= $minwrtsz[$i]) { 
     #now get the details by order of sunum
     $cmd = "$ARCHPROBE jsoc_sums hmidb 5434 production raw group ap ' ' $group 1";
     @datsunum = `$cmd`;
-    $btotal = 0; $ttotal = 0;
-    $j = 0;
-    $filename = "$ARCHFILEDIR/archfile.group$group";
-    open(AR, ">$filename") || die "Can't open $filename: $!\n";
-    print AR "#group series sunum sudir AP (bytes) sumid eff_date\n";
-    #print "@datsunum\n";
+    $seriessave = "NONE";
+    print "raw group $group:\n";
+    print "@datsunum\n";
     #A line looks like
 #group series sunum sudir AP (bytes) sumid eff_date
 #6 hmi.hmiserieslev1pb45 153752265 /SUM1/D153752265 7763370790 62825881 201107041532
@@ -137,159 +227,123 @@ while(($group = shift(@groups)) != -1) {
       if(/^#/ || /^\n/) { #ignore any comment or blank lines
         next;
       }
-      #accumulate 500MB for a tape file
+      #make an array for each seperate series
+      chomp();
       ($grp,$ser,$sunum,$sudir,$bytes,$sumid,$edate) = split(/\s+/);
-      chomp($edate);
-      $btotal += $bytes;
-      push(@fileinfo, $_);
-      if($btotal >= 536870912) { #500MB
+      if($seriessave eq "NONE") {
+        $seriessave = $ser;
+        $sarrayname = "S_$ser";
+        $sarrayname =~ s/\.//;		#elim any '.' before eval
+        push(@sarrayarray, $sarrayname);
+      }
+      if($seriessave eq $ser) {     #continue on same series
+        $cmd = "push(\@$sarrayname, \"$_\")";
+        eval($cmd);
+      }
+      else {			    #start a new series
+          $seriessave = $ser;
+        $sarrayname = "S_$ser";
+        $sarrayname =~ s/\.//;          #elim any '.' before eval
+        push(@sarrayarray, $sarrayname);
+        $cmd = "push(\@$sarrayname, \"$_\")";
+        eval($cmd);
+      }
+    }
+  }
+
+#Now process the arrays for each ds name and make a manifest file for
+#each group. The ds name arrays are in group# order in @sarrayarray.
+$savegrp = -1;
+$filename = 0;
+@pendcmds = ();			#cmd to run after a child completes
+while($x = shift(@sarrayarray)) {
+  print "$x\n";
+  $cmd = "\$_ = shift(\@$x)";
+  eval($cmd);
+  while($_) {
+    print "$_\n";
+    ($grp,$ser,$sunum,$sudir,$bytes,$sumid,$edate) = split(/\s+/);
+    if($grp != $savegrp) {
+      $savegrp = $grp;
+      if($filename) { 
+        close(AR);
+        print "Close $filename\n";
+        $cmd = "/home/production/cvs/JSOC/bin/linux_ia64/tapearcX $filename";
+        if(!(&forkarc($cmd))) {
+          print "Exceeded max forks of $MAXFORKS. Will process later.\n";
+          push(@pendcmds, $cmd);
+        }
+      }
+      $filename = "$ARCHFILEDIR/manifest.group$grp";
+      open(AR, ">$filename") || die "Can't open $filename: $!\n";
+      print AR "#group=$grp Only one group# per manifest file\n";
+      print AR "#group series sunum sudir AP (bytes) sumid eff_date\n";
+      $btotal = 0; $ttotal = 0;
+      $j = 0;
+      $numsudir = 0; 
+      @fileinfo = ();
+    }
+    else {
+      if($ser ne $saveser) {
+      }
+    }
+    #accumulate 500MB or 512 sudir for a tape file and put in manifest
+    $btotal += $bytes;
+    push(@fileinfo, "$_\n");
+    $numsudir++;
+      if(($btotal >= 536870912) || ($numsudir >= 512)) { #500MB or 512 sunum
         print AR "#File $j:\n";
-        print AR "@fileinfo";
+        $info = sprintf("#Total bytes in file = %12s. Num sudir = $numsudir\n",
+                        commify(int($btotal)));
+        print AR "$info";
+        if(!@fileinfo) { 
+          print AR "\n";		#tapearcX requires
+        } else {
+          print AR " @fileinfo";
+        }
         $ttotal += $btotal;
         if($ttotal >= 760000000000) { #!!TBD use cfg info
-          print AR "EOT:\n";
+          #print AR "EOT:\n";	#NO, causes tapearcX problem
           $ttotal = 0;
         }
         $btotal = 0;
+        $numsudir = 0;
         @fileinfo = ();
         $j++;
       }
-    }
-    close(AR);
+    $cmd = "\$_ = shift(\@$x)";
+    eval($cmd);
   }
-  $i++;
+  #have a new ds name. print out the last tape file for the prev ds name
+    print AR "#File $j:\n";
+    $info = sprintf("#Total bytes in file = %12s. Num sudir = $numsudir\n",
+                        commify(int($btotal)));
+    print AR "$info";
+    if(!@fileinfo) { 
+      print AR "\n";		#tapearcX requires
+    } else {
+      print AR " @fileinfo";
+    }
+    $ttotal += $btotal;
+    if($ttotal >= 760000000000) { #!!TBD use cfg info
+      #print AR "EOT:\n";	#NO, causes tapearcX problem
+      $ttotal = 0;
+    }
+    $btotal = 0;
+    $numsudir = 0;
+    @fileinfo = ();
+    $j++;
 }
-exit;
-
-##################OLD STUFF BELOW########################################
-#First connect to database
-  $dbh = DBI->connect("dbi:Pg:dbname=$DB;host=$hostdb;port=$PGPORT", "$user", "");
-  if ( !defined $dbh ) {
-    die "Cannot do \$dbh->connect: $DBI::errstr\n";
+if($filename) { 
+  close(AR);
+  print "Close $filename\n";
+  $cmd = "/home/production/cvs/JSOC/bin/linux_ia64/tapearcX $filename";
+  if(!(&forkarc($cmd))) {
+    print "Exceeded max forks of $MAXFORKS\n";
+    push(@pendcmds, $cmd);
   }
-
-  print "	Rounded down to nearest Megabyte\n";
-  print "	NOTE: DP<=100d includes DPnow\n";
-  print "Query in progress, may take awhile...\n";
-  printf("Group %12s %12s %12s %12s\n", "DPnow", "DP<=100d", "DPlater", "AP");
-  printf("----- %12s %12s %12s %12s\n", "--------", "--------", "--------", "--------");
-
-    @groupids = (0,1,2,3,4,5,6,7,8,9,10,11,12,102,103,104,105,310,311,400,401,10000,-1);
-    while(($group = shift(@groupids)) != -1) {
-      if($FAST) {
-        $sql = "select sum(bytes) from sum_partn_alloc where status=2 and group_id=$group and effective_date <= '$effdate'";
-      } 
-      else {  #there can be data from tape that is always in group=0
-        $sql = "select sum(bytes) from sum_main where storage_group=$group and ds_index in (select ds_index from sum_partn_alloc where status=2 and effective_date <= '$effdate')";
-      }
-      #print "$sql\n"; #!!!TEMP
-      $sth = $dbh->prepare($sql);
-      if ( !defined $sth ) {
-        print "Cannot prepare statement: $DBI::errstr\n";
-        $dbh->disconnect();
-        exit; 
-      }
-      $sth->execute;
-      while ( @row = $sth->fetchrow() ) {
-        $bytes = shift(@row);
-        $bytes = $bytes/1048576;
-        push(@bytes, $bytes);
-      }
-
-      #now get DP<=100d
-      if($FAST) {
-        $sql = "select sum(bytes) from sum_partn_alloc where status=2 and group_id=$group and effective_date <= '$xeffdate'";
-      }
-      else {
-        $sql = "select sum(bytes) from sum_main where storage_group=$group and ds_index in (select ds_index from sum_partn_alloc where status=2 and effective_date <= '$xeffdate')";
-      }
-      #print "$sql\n"; #!!!TEMP
-      $sth = $dbh->prepare($sql);
-      if ( !defined $sth ) {
-        print "Cannot prepare statement: $DBI::errstr\n";
-        $dbh->disconnect();
-        exit; 
-      }
-      $sth->execute;
-      while ( @row = $sth->fetchrow() ) {
-        $bytes100 = shift(@row);
-        $bytes100 = $bytes100/1048576;
-        push(@bytes100, $bytes100);
-      }
-      #now get DPlater 
-      if($FAST) {
-        $sql = "select sum(bytes) from sum_partn_alloc where status=2 and group_id=$group and effective_date > '$xeffdate'";
-      }
-      else {
-        $sql = "select sum(bytes) from sum_main where storage_group=$group and ds_index in (select ds_index from sum_partn_alloc where status=2 and effective_date > '$xeffdate')";
-      }
-      #print "$sql\n"; #!!!TEMP
-      $sth = $dbh->prepare($sql);
-      if ( !defined $sth ) {
-        print "Cannot prepare statement: $DBI::errstr\n";
-        $dbh->disconnect();
-        exit; 
-      }
-      $sth->execute;
-      while ( @row = $sth->fetchrow() ) {
-        $byteso = shift(@row);
-        $byteso = $byteso/1048576;
-        push(@byteso, $byteso);
-      }
-      #now get AP 
-      $sql = "select sum(bytes) from sum_partn_alloc where status=4 and archive_substatus=128 and group_id=$group";
-      #print "$sql\n"; #!!!TEMP
-      $sth = $dbh->prepare($sql);
-      if ( !defined $sth ) {
-        print "Cannot prepare statement: $DBI::errstr\n";
-        $dbh->disconnect();
-        exit; 
-      }
-      $sth->execute;
-      while ( @row = $sth->fetchrow() ) {
-        $bytesap = shift(@row);
-        $bytesap = $bytesap/1048576;
-        $totalbytesap += $bytesap;
-        $bytes = shift(@bytes);
-        $totalbytes += $bytes;
-        $avail = shift(@avail);
-        $totalavail += $avail;
-        $bytes100 = shift(@bytes100);
-        $totalbytes100 += $bytes100;
-        $byteso = shift(@byteso);
-        $totalbyteso += $byteso;
-        #printf("$sum %12d %12d %12d %12d\n", $avail,$bytes,$byteso,$bytesap);
-        printf("$group\t%12s %12s %12s %12s\n", 
-		commify(int($bytes)), commify(int($bytes100)), 
-		commify(int($byteso)), commify(int($bytesap)));
-      }
-    }
-    printf("------------------------------------------------------------\n");
-    printf("TOTAL:\t%12s %12s %12s %12s\n", 
-		commify(int($totalbytes)), commify(int($totalbytes100)),
-		commify(int($totalbyteso)), commify(int($totalbytesap)));
-
-    $totalstor = $totalbytes+$totalbytes100+$totalbyteso+$totalbytesap;
-
-    #Get all storage that is read back from tape  
-    $sql = "select sum(bytes) from sum_partn_alloc where group_id=0 and wd like '/%/D%/D%'";
-    #print "$sql\n"; #!!!TEMP
-    $sth = $dbh->prepare($sql);
-    if ( !defined $sth ) {
-      print "Cannot prepare statement: $DBI::errstr\n";
-      $dbh->disconnect();
-      exit; 
-    }
-    $sth->execute;
-    while ( @row = $sth->fetchrow() ) {
-      $bytest = shift(@row);
-      $bytest = $bytest/1048576;
-    }
-
-    $p = sprintf("%02.1f", 100.0 * ($bytest/$totalstor));
-    #print "bytest=$bytest totalstor=$totalstor\n";
-    print "\nTotal bytes online that have been read back from tape:\n";
-    printf "%12s = $p percent of total storage of %12s\n", commify(int($bytest)), commify(int($totalstor));
-
-$dbh->disconnect();
+}
+while(1) { 
+  sleep 10; 
+}
 
