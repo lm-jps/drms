2d069d350c8e5d4590a0580188e7e918f79c8526
arta <arta>
2011-06-02 23:31:27 +0000

    Update tag for the 5.13 JSOC release.

--- base/jsoc_version.h
+++ base/jsoc_version.h
@@ -13,8 +13,8 @@
 
 #define JSOC_VERSION_INCL 1
 
-#define jsoc_version "V5R12X"
-#define jsoc_vers_num (-512)
+#define jsoc_version "V5R13"
+#define jsoc_vers_num (513)
 
 
 static inline const char *jsoc_getversion(char *verstr, int size, int *isdev)
