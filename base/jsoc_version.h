4e78d80049aa6c1b7e3f0364643546074aff79b6
arta <arta>
2011-11-17 00:36:54 +0000

    Bump to 6.0 for next release.

--- base/jsoc_version.h
+++ base/jsoc_version.h
@@ -13,8 +13,8 @@
 
 #define JSOC_VERSION_INCL 1
 
-#define jsoc_version "V5R14X"
-#define jsoc_vers_num (-514)
+#define jsoc_version "V6R0"
+#define jsoc_vers_num (600)
 
 
 static inline const char *jsoc_getversion(char *verstr, int size, int *isdev)
