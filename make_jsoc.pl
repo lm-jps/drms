712514c397ee1c002ea1ad54ee4c457cd638adba
arta <arta>
2012-02-08 23:58:44 +0000

    Dont cd in this script! The caller of this script has already done that.

--- make_jsoc.pl
+++ make_jsoc.pl
@@ -3,27 +3,14 @@
 # run this on each machine to be used.
 #    n00 - for linux_ia32 machines such as n00, ..., n11, etc.
 #    n12 - for linux_x86_64 machines
+# 
+# *** The caller of this script must cd to the JSOC root directory before running this script. ***
 
 use Cwd qw(realpath);
 
-my($scriptpath) = realpath($0);
-print STDOUT "make of JSOC $scriptpath\n";
+my($cdir) = realpath($ENV{'PWD'});
+print STDOUT "make of JSOC $cdir\n";
 system("date");
-
-my($wd);
-$wd = $scriptpath;
-
-if ($wd =~ /(.+)\/\S+$/)
-{
-    $wd = $1;
-}
-else
-{
-    print STDERR "Invalid make_jsoc.pl path; bailing!\n";
-    exit(1);
-}
-
-chdir($wd);
 
 # make clean 
 # make -j 4 
