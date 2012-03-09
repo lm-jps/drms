8aa2592a8972ca587d1d2785c10dd4ed96a2baa0
arta <arta>
2012-03-09 23:21:16 +0000

    Publication perl scripts.

--- base/drms/replication/toolbox.pm
+++ base/drms/replication/toolbox.pm
@@ -6,6 +6,8 @@ package toolbox;
 #
 
 use Fcntl ':flock';
+use Getopt::Long;
+use FileHandle;
 require 'timelocal.pl';
 
 my(%fpaths);
@@ -191,7 +193,7 @@ sub AcquireLock
    my($lckfh) = $_[1];
    my($gotlock);
    my($natt);
-
+    
    if (-e $path)
    {
       $$lckfh = FileHandle->new("<$path");
@@ -247,6 +249,119 @@ sub ReleaseLock
       chmod(0664, $lckfpath);
       delete($fpaths{$lckfn});
    }
+}
+
+# Given a list of expected arguments, GetArgs() parses the cmd-line and captures the expected-argument values.
+# If an expected argument is missing, then GetArgs() returns with an error.
+# argsinH -  Input: reference to hash array whose keys are the argument names (as specified in GetOptions()), and whose 
+#            values are the argument type (as specified in GetOptions()).
+# argsoutH - Output: reference to hash array whose keys are the argument names (as specified in GetOptions()), and whose 
+#            values are the argument values as specified on the cmd-line.
+#
+# Returns 0 on error, 1 on success. Will return error if the requested argument name does not exist on the cmd-line.
+# 
+# NOTE: Each cmd-line argument name must match exactly one of the argument names in argsinH. No argument name
+# can begin with '-' (GetOptions() considers that an invalid argument name).
+sub GetArgs
+{
+   my($argsinH) = $_[0];
+   my($argsoutH) = $_[1];
+
+   my($rv) = 0;
+   my(@argsA);
+   my(@optlist);
+   my($iarg);
+   my($type);
+   my($rsp);
+
+   # Create hash array to pass to GetOptions
+   @argsA = keys(%$argsinH);
+   foreach $iarg (@argsA)
+    {
+        $type = $argsinH->{$iarg};
+        
+        if ($type =~ /noval/i)
+        {
+            push(@optlist, "$iarg");
+        }
+        else
+        {    
+            push(@optlist, "$iarg=$type");
+        }
+    }
+
+   # Trick to get GetOptions() to ignore what we'd consider options. If an argument
+   # name, from the cmd-line, starts with an optional space, then it matches, and 
+   # GetOptions() will consider it an option and process it. It will also accept
+   # things that start with '-', but in the @optlist, no argument will start with '-'.
+   # If we want this to work for options too, defined as arguments that start with
+   # '-', then each option in @optlist should start with '-'.
+   # ( |) works for some reason too.
+   # pass_through allows the skipping of unknown options (instead of causing GetOptions()
+   # to fail.
+   Getopt::Long::Configure("prefix_pattern=( ?)", "pass_through");
+
+   # Returns undef if any options on cmd-line are not in @optlist. Returns 1 otherwise, even if
+   # there is an item in @optlist that is not on the cmd-line.
+   $rsp = GetOptions($argsoutH, @optlist);
+
+   $rv = $rsp if (defined($rsp));
+
+   if ($rv)
+   {
+      # Now check that all arguments in argsinH were actually specified on the cmd-line.
+      my(@check);
+
+      @argsA = keys(%$argsinH);
+      @check = map ({(!defined($argsoutH->{$_}) ? $_ : ())} @argsA);
+      $rv = ($#check < 0);
+
+      unless ($rv)
+      {
+         my($msg) = join(', ', @check);
+         print STDERR "Missing required argument(s) '$msg'.\n";
+      }
+   }
+
+   return $rv;
+}
+
+sub GetOpts
+{
+    my($argsinH) = $_[0];
+    my($argsoutH) = $_[1];
+    
+    my($rv) = 0;
+    my(@argsA);
+    my(@optlist);
+    my($iarg);
+    my($type);
+    my($rsp);
+    
+    # Create hash array to pass to GetOptions
+    @argsA = keys(%$argsinH);
+    foreach $iarg (@argsA)
+    {
+        $type = $argsinH->{$iarg}; 
+        if ($type =~ /noval/i)
+        {
+            push(@optlist, "$iarg");
+        }
+        else
+        {
+            push(@optlist, "$iarg=$type");            
+        }
+    }
+    
+    # A previous Configure call could mess everything up. Just always precede GetOptions()
+    # with the proper configuration.
+    Getopt::Long::Configure("default");
+    
+    $rsp = GetOptions($argsoutH, @optlist);
+    
+    $rv = $rsp if (defined($rsp));
+    
+    return $rv;
 }
 
 1;
