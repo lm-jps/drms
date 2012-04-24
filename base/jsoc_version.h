afc8e246f05b36e56cf4259f57f9bbe38ed86452
arta <arta>
2012-04-24 22:27:25 +0000

    Change version number to development format.

--- base/jsoc_version.h
+++ base/jsoc_version.h
@@ -13,8 +13,8 @@
 
 #define JSOC_VERSION_INCL 1
 
-#define jsoc_version "V6R2"
-#define jsoc_vers_num (602)
+#define jsoc_version "V6R2X"
+#define jsoc_vers_num (-602)
 
 
 static inline const char *jsoc_getversion(char *verstr, int size, int *isdev)
