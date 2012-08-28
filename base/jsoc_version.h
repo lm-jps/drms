af0a308a092c306d91140e99e998832960aa05c1
arta <arta>
2012-08-28 21:13:51 +0000

    Bump to 7.0 for next release.

--- base/jsoc_version.h
+++ base/jsoc_version.h
@@ -13,48 +13,48 @@
 
 #define JSOC_VERSION_INCL 1
 
-#define jsoc_version "V6R4X"
-#define jsoc_vers_num (-604)
+#define jsoc_version "V7R0"
+#define jsoc_vers_num (700)
 
 
 static inline const char *jsoc_getversion(char *verstr, int size, int *isdev)
 {
-   char *vers = strdup(jsoc_version);
-   char *pc = NULL;
-   int len = strlen(jsoc_version);
-   
-   if (vers)
-   {
-      if (isdev)
-      {
-         *isdev = 0;
-      }
-      
-      if ((pc = strchr(vers, 'R')) != NULL)
-      {
-         *pc = '\0';
-      }
-      
-      if (jsoc_version[len - 1] == 'X')
-      {
-         if (isdev)
-         {
-            *isdev = 1;
-         }
-         
-         vers[len - 1] = '\0';
-      }
-      
-      snprintf(verstr, size, "%s.%s", vers + 1, pc + 1);
-      
-      free(vers);
-   }
-   else
-   {
-      fprintf(stderr, "Out of memory.\n");
-   }  
-   
-   return jsoc_version;
+    char *vers = strdup(jsoc_version);
+    char *pc = NULL;
+    int len = strlen(jsoc_version);
+    
+    if (vers)
+    {
+        if (isdev)
+        {
+            *isdev = 0;
+        }
+        
+        if ((pc = strchr(vers, 'R')) != NULL)
+        {
+            *pc = '\0';
+        }
+        
+        if (jsoc_version[len - 1] == 'X')
+        {
+            if (isdev)
+            {
+                *isdev = 1;
+            }
+            
+            vers[len - 1] = '\0';
+        }
+        
+        snprintf(verstr, size, "%s.%s", vers + 1, pc + 1);
+        
+        free(vers);
+    }
+    else
+    {
+        fprintf(stderr, "Out of memory.\n");
+    }  
+    
+    return jsoc_version;
 }
 
 #endif
