dff077561a163e9366148e5c27595386be16108e
arta <arta>
2013-02-26 20:22:15 +0000

    Add jsmn to the list of directories that make makes an output directory for.

--- target.mk
+++ target.mk
@@ -53,6 +53,7 @@ $(OBJDIR):
 	+@[ -d $@/base/libs/defs/fpic ] || mkdir -p $@/base/libs/defs/fpic
 	+@[ -d $@/base/libs/dstruct/fpic ] || mkdir -p $@/base/libs/dstruct/fpic
 	+@[ -d $@/base/libs/json ] || mkdir -p $@/base/libs/json
+	+@[ -d $@/base/libs/jsmn ] || mkdir -p $@/base/libs/jsmn
 	+@[ -d $@/base/libs/inthandles ] || mkdir -p $@/base/libs/inthandles
 	+@[ -d $@/base/libs/qdecoder/md5 ] || mkdir -p $@/base/libs/qdecoder/md5
 	+@[ -d $@/base/libs/threads/fpic ] || mkdir -p $@/base/libs/threads/fpic
