cde4e9e1102ac144b7ff03bae37f1f8dc266f644
arta <arta>
2013-09-05 18:19:12 +0000

    Try not capturing stdout of dumpreptables.pl. Instead, just dumpreptables.pl print to stdout, which should then get redirected to the sql file.

--- base/drms/replication/subscribe_manage/sdo_slony1_dump.sh
+++ base/drms/replication/subscribe_manage/sdo_slony1_dump.sh
@@ -2,7 +2,7 @@
 # ----------
 # slony1_dump.sh
 #
-# $Id: sdo_slony1_dump.sh,v 1.9 2013/09/05 17:22:49 arta Exp $
+# $Id: sdo_slony1_dump.sh,v 1.10 2013/09/05 18:19:12 arta Exp $
 #
 #	This script creates a special data only dump from a subscriber
 #	node. The stdout of this script, fed into psql for a database that
@@ -260,8 +260,7 @@ done
 # The stdout of from this call will contain SQL that runs on the client end. It dumps the series tables.
 cmd="$kJSOCRoot/base/drms/replication/subscribe_manage/dumpreptables.pl config=$kJSOCRoot/proj/replication/etc/repserver.cfg client=$node sublist=$sublist idlist=$idlist newcl=$new_subscriber filectr=$output_filecounter 2>>$SMworkDir/slony1_dump.$node.log"
 echo "Executing $cmd" >$SMworkDir/slony1_dump.$node.log
-toprint=`$cmd`
-echo "$toprint"
+$cmd
 
 # ----
 # Emit the commit for the dump to stdout.
