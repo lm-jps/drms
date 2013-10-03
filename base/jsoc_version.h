43c1ad673b7ce5c1637bbcfb5cc54d2b3df2db32
arta <arta>
2013-10-03 14:18:51 +0000

    Set the development version of the version macros for the 8.1 release./home/arta/jsoctrees/JSOC/base/jsoc_version.h

--- base/jsoc_version.h
+++ base/jsoc_version.h
@@ -13,8 +13,8 @@
 
 #define JSOC_VERSION_INCL 1
 
-#define jsoc_version "V8R1"
-#define jsoc_vers_num (801)
+#define jsoc_version "V8R1X"
+#define jsoc_vers_num (-801)
 
 
 static inline const char *jsoc_getversion(char *verstr, int size, int *isdev)
