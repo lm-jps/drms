87eecb179d4ca30f111a039e15ed907edb829283
arta <arta>
2013-11-15 17:05:38 +0000

    Make this work with the configuration changes for SUMS.

--- base/libs/perl/JSOC_DB_INFO_INCLUDE.pl
+++ base/libs/perl/JSOC_DB_INFO_INCLUDE.pl
@@ -1,3 +1,10 @@
+#!/home/jsoc/bin/linux_x86_64/activeperl 
+
 #This is widely included by perl scripts
-$JSOC_DB_HOST = "hmidb";
+#$JSOC_DB_HOST = "hmidb";
 #$JSOC_DB_HOST = "hmidb2";
+package drmsparams;
+
+use constant SUMS_DB_HOST => "hmidb";
+
+1;
