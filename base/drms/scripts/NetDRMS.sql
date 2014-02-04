36ec3a9d4cc67794ce07ad5761bd6c5c59a06bf4
arta <arta>
2014-02-04 19:17:08 +0000

    Add commands to create the drms namespace and the sumsadmin db user.

--- base/drms/scripts/NetDRMS.sql
+++ base/drms/scripts/NetDRMS.sql
@@ -1,5 +1,7 @@
-/* base/drms/scripts/NetDRMS.sql */
+-- base/drms/scripts/NetDRMS.sql
+-- This script creates various database objects that partially compose DRMS
 
+-- admin tables
 create schema admin;
 grant usage on schema "admin" to public;
 
@@ -16,3 +18,12 @@ create table admin.sessionns (
 
 grant select on "admin".sessionns to public;
 grant select on "admin".ns to public;
+
+-- Create the drms schema. It has both required and optional tables.
+CREATE SCHEMA drms;
+GRANT USAGE ON SCHEMA drms TO public;
+
+-- This user is hard-coded in the DRMS C source, so it must exist. 
+-- The PostgreSQL administrator account (e.g., "production") is actually an alias for this user, which 
+-- has elevated privileges.
+CREATE USER SUMSADMIN;
