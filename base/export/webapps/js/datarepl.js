c99f94a3ef221eeaa8747a67d10bf0e1415cd022
arta <arta>
2014-04-14 14:06:06 +0000

    Beginnings of a site to manage data replication.

---
+++ base/export/webapps/js/datarepl.js
@@ -0,0 +1,26 @@
+<!--Download the complete list of published series-->
+
+
+
+<!--Called when the DOM has completed loading.-->
+$(document).ready(function()
+{
+    alert("the eagle has landed");
+                  
+                  $(function() {
+                      alert("does this execute?");
+                  });
+
+
+    $(function() {
+                    $("#publist").selectable();
+                    });
+                  
+    $("#date").datepicker();
+
+               
+                  
+    
+                  
+});
+
