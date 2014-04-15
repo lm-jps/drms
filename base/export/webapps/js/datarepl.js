1f97fc898511b40fd77e7f2f62daf9a82304478a
arta <arta>
2014-04-15 02:00:10 +0000

    Dynamically create the list of published series by calling publist.py, which was recently modified to return json in a cgi context.

--- base/export/webapps/js/datarepl.js
+++ base/export/webapps/js/datarepl.js
@@ -1,24 +1,47 @@
 <!--Download the complete list of published series-->
+var gCodeTree = '/home/jsoc/cvs/Development/JSOC'
+// var gCgiBinBaseUrl = '/Volumes/arta/jsoctrees/JSOC/base/drms/replication/publishseries';
+// var gCgiBinBaseUrl = '../../cgi-bin/ajax';
+var gCgiBinBaseUrl = 'http://jsoc2.stanford.edu/cgi-bin/ajax';
 
 
+function listPopulator(listObj, list)
+{
+    for (var series in list)
+    {
+        listObj.append('<li class="ui-widget-content ui-state-default">' + list[series] + '</li>');
+    }
+};
 
 <!--Called when the DOM has completed loading.-->
 $(document).ready(function()
 {
+    var pubListUrl = gCgiBinBaseUrl + '/publist.py';
+    var cfgFile = gCodeTree + '/proj/replication/etc/repserver.cfg';
+                  
     alert("the eagle has landed");
                   
-                  $(function() {
-                      alert("does this execute?");
-                  });
+    // Everything having to do with the publist select control.
+    $(function()
+    {
+        $("#publist").selectable();
 
+        // Fetch the list of published series from the PostgreSQL database.
+        $.ajax(
+        {
+            url: pubListUrl,
+            data: {"c": cfgFile},
+            success: function(data, textStatus, xhr){ var plc = new listPopulator($("#publist"), data.publist.list); },
+            error: function(xhr, textStatus, errorThrown){ alert('Something done went wrong ' + textStatus); },
+            dataType: 'json'
+        });
+               
 
-    $(function() {
-                    $("#publist").selectable();
-                    });
+    });
                   
     $("#date").datepicker();
 
-               
+
                   
     
                   
