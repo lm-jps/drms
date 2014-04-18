6c79595a20da82193742eb436c1f1258c9bb6938
arta <arta>
2014-04-18 20:29:13 +0000

    Make the publist element single-select. Pass the series description to the javascript.

--- base/export/webapps/js/datarepl.js
+++ base/export/webapps/js/datarepl.js
@@ -5,11 +5,18 @@ var gCodeTree = '/home/jsoc/cvs/Development/JSOC'
 var gCgiBinBaseUrl = 'http://jsoc2.stanford.edu/cgi-bin/ajax';
 
 
-function listPopulator(listObj, list)
+function seriesDict(listObj, list)
 {
+    this.dict = new Array();
+    this.getDesc = function(series){ return this.dict[series]; };
+
     for (var series in list)
     {
+	// Create a list item for each series.
         listObj.append('<li class="ui-widget-content ui-state-default">' + list[series][0] + '</li>');
+
+	// Map series name to description.
+	this.dict[list[series][0]] = list[series][1];
     }
 };
 
@@ -18,6 +25,7 @@ $(document).ready(function()
 {
     var pubListUrl = gCgiBinBaseUrl + '/publist.py';
     var cfgFile = gCodeTree + '/proj/replication/etc/repserver.cfg';
+    var sDict;
                   
     alert("the eagle has landed");
                   
@@ -25,14 +33,23 @@ $(document).ready(function()
     $(function()
     {
         $("#publist").selectable(
-      );
+        {
+            selected: function(event, ui) 
+            {
+		// Unselect all other items (making this a single-select selectable control
+                $(ui.selected).siblings().removeClass("ui-selected");
 
+		// Display the selected series' description
+		
+            } 
+        });
+        
         // Fetch the list of published series from the PostgreSQL database.
         $.ajax(
         {
             url: pubListUrl,
             data: {"c": cfgFile},
-            success: function(data, textStatus, xhr){ var plc = new listPopulator($("#publist"), data.publist.list); },
+            success: function(data, textStatus, xhr){ sDict = new seriesDict($("#publist"), data.publist.list); },
             error: function(xhr, textStatus, errorThrown){ alert('Something done went wrong ' + textStatus); },
             dataType: 'json'
         });
