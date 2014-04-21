4a17a828e7be1b0e3179032772afe107c38c3a25
arta <arta>
2014-04-21 17:18:22 +0000

    Update series description after the user has selected the series. Add a shadow to accordion.

--- base/export/webapps/js/datarepl.js
+++ base/export/webapps/js/datarepl.js
@@ -9,14 +9,22 @@ function seriesDict(listObj, list)
 {
     this.dict = new Array();
     this.getDesc = function(series){ return this.dict[series]; };
+    this.getSelected = function(){ return this.selected; };
+    this.setSelected = function(series)
+                       {
+                            if (this.getDesc(series) != undefined)
+                            {
+                                this.selected = series;
+                            }
+                       };
 
     for (var series in list)
     {
-	// Create a list item for each series.
+        // Create a list item for each series.
         listObj.append('<li class="ui-widget-content ui-state-default">' + list[series][0] + '</li>');
 
-	// Map series name to description.
-	this.dict[list[series][0]] = list[series][1];
+        // Map series name to description.
+        this.dict[list[series][0]] = list[series][1];
     }
 };
 
@@ -36,19 +44,36 @@ $(document).ready(function()
         {
             selected: function(event, ui) 
             {
-		// Unselect all other items (making this a single-select selectable control
+                // Unselect all other items (making this a single-select selectable control
                 $(ui.selected).siblings().removeClass("ui-selected");
 
-		// Display the selected series' description
-		
-            } 
+                // Display the selected series' description
+                if (sDict != undefined)
+                {
+                    sDict.setSelected($(ui.selected).text());
+                }
+            },
+                                 
+            stop: function(event, ui)
+            {
+                if (sDict != undefined)
+                {
+                    var series = sDict.getSelected();
+                    var desc = sDict.getDesc(series);
+                                 
+                    if (desc != undefined)
+                    {
+                        $("#series-desc").text(desc);
+                    }
+                }
+            }
         });
         
         // Fetch the list of published series from the PostgreSQL database.
         $.ajax(
         {
             url: pubListUrl,
-            data: {"c": cfgFile},
+            data: {"c" : cfgFile, "d" : 1},
             success: function(data, textStatus, xhr){ sDict = new seriesDict($("#publist"), data.publist.list); },
             error: function(xhr, textStatus, errorThrown){ alert('Something done went wrong ' + textStatus); },
             dataType: 'json'
