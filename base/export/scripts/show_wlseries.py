51d53c357cc6fc61fc7a422a64c38831aa310bfc
arta <arta>
2014-09-08 18:16:39 +0000

    Check-in whatever I was working on months ago.

--- base/export/scripts/show_wlseries.py
+++ base/export/scripts/show_wlseries.py
@@ -21,7 +21,7 @@ RET_INVALIDARG = 1
 RET_DBCONNECT = 2
 
 def getUsage():
-    return 'show_wlseries.py intdsn=<internal-db dsn> [ extdsn=<external-db dsn> wltab=<white-list table> ] [ filter=<series name filter> ]\n  dsn=dbname:<db name>;dbhost=<hostname>;dbport=<port number>[;dbuser=<PG user>]'
+    return 'show_wlseries.py intdsn=<internal-db dsn> [ extdsn=<external-db dsn> wltab=<white-list table> ] [ filter=<series name filter> ]\n\n  (where dsn=dbname:<db name>;dbhost=<hostname>;dbport=<port number>[;dbuser=<PG user>])'
 
 def parseArg(options, args, arg, regexp, isList, etype):
     if not arg is None:
@@ -62,7 +62,7 @@ def GetArgs(args):
     # Use REQUEST_URI as surrogate for the invocation coming from a CGI request.
     if os.getenv('REQUEST_URI'):
         optD['source'] = 'cgi'
-        etype = 'getArgsForm'
+        etype = 'getArgsCgi'
     else:
         optD['source'] = 'cmdline'
         etype = 'getArgsCmdline'
@@ -99,7 +99,7 @@ def GetArgs(args):
         # The non-optional arguments are positional. It is possible that the caller could put the arguments for one argument into
         # the position for a different argument. Positional arguments are a nuisance. So we must parse the values of these three arguments
         # and get the name of the arguments from the LHS of the equal sign.
-        regexp = re.compile(r"([^\s=]+)=(\S+)")
+        regexp = re.compile(r"([^\s=]+)\s*=\s*(\S.*)")
         parseArg(optD, args, args.filter, regexp, False, etype)
         parseArg(optD, args, args.intdsn, regexp, False, etype)
         parseArg(optD, args, args.extdsn, regexp, False, etype)
@@ -117,11 +117,59 @@ def GetArgs(args):
         optD['extdsn'] = parseDSN(optD['extdsn'], etype)
     else:
         optD['extdsn'] = None
-    
+
+    # If any optional arguments were not present, set them to None in the options dictionary.
+    if not 'filter' in optD:
+        optD['filter'] = None
+    if not 'wltab' in optD:
+        optD['wltab'] = None
+
     return optD
 
 def insertJson(jsonDict, property, value):
     jsonDict[property] = value
+
+def parseFilter(filterStr):
+    # By default, Python matches at the beginning of the string only. NOT must be in all caps.
+    regexp = re.compile(r"\s*NOT\s+(\S.*)")
+    matchobj = regexp.match(filterStr)
+    if matchobj is not None:
+        exclude = True
+        remainder = matchobj.group(1)
+        print('really')
+    else:
+        print('bummer ' + filterStr)
+        exclude = False
+        remainder = filterStr
+
+    # Look for a namespace.
+    regexp = re.compile(r"\s*(\S+)(\.\S+)(.*)")
+    matchobj = regexp.match(remainder)
+    if matchobj is not None:
+        namespace = matchobj.group(1)
+        regexpStr = namespace + matchobj.group(2)
+        remainder = matchobj.group(3)
+    else:
+        namespace = None
+        regexp = re.compile(r"\s*(\S+)(.*)")
+        matchobj = regexp.match(remainder)
+        if matchobj is not None:
+            regexpStr = matchobj.group(1)
+            remainder = matchobj.group(2)
+        else:
+            regexpStr = ''
+            remainder = ''
+
+    # Ensure there are no non-white-space chars in remainder.
+    regexp = re.compile(r"\s*(\S+)")
+    matchobj = regexp.match(remainder)
+    if matchobj is not None:
+        # Bad syntax
+        raise Exception('parseFilter', 'Invaid filter ' + filterStr)
+
+    filter = (namespace, exclude, regexpStr)
+
+    return filter
         
 rv = RET_SUCCESS
 
@@ -132,7 +180,7 @@ if __name__ == "__main__":
         optD = GetArgs(sys.argv[1:])
         if optD is None:
             # Return JSON just in case this script was initiated by web.
-            raise Exception('getArgsForm', 'No arguments provided.')
+            raise Exception('getArgsCgi', 'No arguments provided.')
     
     except Exception as exc:
         if len(exc.args) != 2:
@@ -141,19 +189,22 @@ if __name__ == "__main__":
         etype = exc.args[0]
         msg = exc.args[1]
         
-        if etype == 'getArgsForm':
+        if etype == 'getArgsCgi':
             rootObj = {}
-            listObj = {}
-            insertJson(listObj, 'errMsg', msg)
-            insertJson(rootObj, 'publist', listObj)
+            listObj = []
+            insertJson(rootObj, 'seriesList', listObj) # An array
+            insertJson(rootObj, 'errMsg', msg) # A string
+
             print('Content-type: application/json\n')
             print(json.dumps(rootObj))
-            optD['source'] = 'form'
+            optD['source'] = 'cgi'
         elif etype == 'getArgsCmdline':
             print(msg)
             print('Usage:\n  ' + getUsage())
             optD['source'] = 'cmdline'
             rv = RET_INVALIDARG
+        else:
+            raise # Re-raise
 
 if rv == RET_SUCCESS:
     # Connect to the database
@@ -161,7 +212,31 @@ if rv == RET_SUCCESS:
         # The connection is NOT in autocommit mode. If changes need to be saved, then conn.commit() must be called.
         with psycopg2.connect(database=optD['intdsn']['dbname'], user=optD['intdsn']['dbuser'], host=optD['intdsn']['dbhost'], port=optD['intdsn']['dbport']) as conn:
             with conn.cursor() as cursor:
-                cmd = "SELECT seriesname FROM drms_series() WHERE seriesname ~ 'su_arta'"
+                # The filter is an extended POSIX regular expression, optionally preceded by the word NOT.
+                # filter is a tuple. The first element is a namespace (if not None, then search in this namespace only).
+                # The second is a boolean. If True, then the filter specifies which series to exclude. If False,
+                # then the filter specifies with series to include.
+                # The third is a regular expression to send to PG that, in combination with the second element,
+                # selects series to print. This element can be None, in which case all series are printed.
+                if optD['filter'] is not None:
+                    filter = parseFilter(optD['filter'])
+                    if filter[1]:
+                        matchOp = '!~*'
+                    else:
+                        matchOp = '~*'
+                
+                    if filter[2] is None:
+                        cmd = 'SELECT seriesname FROM drms_series()'
+                    else:
+                        if filter[0] is not None and not filter[1]:
+                            cmd = 'SELECT seriesname FROM ' + filter[0] + '.drms_series WHERE seriesname ' + matchOp + " '" + filter[2] + "'"
+                        else:
+                            print('here')
+                            cmd = 'SELECT seriesname FROM drms_series() WHERE seriesname ' + matchOp + " '" + filter[2] + "'"
+                            print(cmd)
+                                
+                else:
+                    cmd = 'SELECT seriesname FROM drms_series()'
 
                 try:
                     cursor.execute(cmd)
@@ -171,7 +246,7 @@ if rv == RET_SUCCESS:
 
                 for record in cursor:
                     print(record[0])
-                
+
     except psycopg2.Error as exc:
         # Closes the cursor and connection
         print('Unable to connect to the database', file=sys.stderr)
@@ -180,3 +255,25 @@ if rv == RET_SUCCESS:
         # No need to close cursor - leaving the with block does that.
         rv = RET_DBCONNECT
 
+    except Exception as exc:
+        if len(exc.args) != 2:
+            raise # Re-raise
+        
+        etype = exc.args[0]
+        msg = exc.args[1]
+
+        if etype == 'parseFilter':
+            if optD['source'] == 'cgi':
+                rootObj = {}
+                listObj = []
+                insertJson(rootObj, 'seriesList', listObj) # An array
+                insertJson(rootObj, 'errMsg', msg) # A string
+            
+                print('Content-type: application/json\n')
+                print(json.dumps(rootObj))
+            elif optD['source'] == 'cmdline':
+                print(msg, file=sys.stderr)
+            else:
+                raise # Re-raise
+        else:
+            raise # Re-raise
