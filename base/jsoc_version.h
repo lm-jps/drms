ea200af4330b603629f75d00429ff0e03ab9958b
arta <arta>
2014-09-26 17:20:22 +0000

    Set the release versions of the version macros for the 8.6 release.

--- base/jsoc_version.h
+++ base/jsoc_version.h
@@ -13,8 +13,8 @@
 
 #define JSOC_VERSION_INCL 1
 
-#define jsoc_version "V8R5X"
-#define jsoc_vers_num (-805)
+#define jsoc_version "V8R6"
+#define jsoc_vers_num (806)
 
 
 static inline const char *jsoc_getversion(char *verstr, int size, int *isdev)
