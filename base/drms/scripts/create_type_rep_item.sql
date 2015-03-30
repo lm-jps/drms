a3aa612cdb31cfd829f3115fd8a304e23f4d3f55
cvs-fast-export <cvs-fast-export>
2015-03-30 14:06:15 +0000

    Synthetic commit for incomplete tag globalhs_version_7

--- base/drms/scripts/create_type_rep_item.sql
+++
@@ -1,7 +0,0 @@
-create type rep_item as (
-    tab_id      int8,
-    tab_relname varchar(255),
-    tab_nspname varchar(255),
-    tab_set     int8,
-    tab_idxname varchar(255),
-    tab_comment text );
