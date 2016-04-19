1a43ddfd5b164470dca5cf4c89aa7425e77676c6
arta <arta>
2016-04-19 19:11:06 +0000

    Move newdrmsuser.pl to base so remote sites can use it; add the CREATE ROLE command for user jsoc to NetDRMS.sql.

--- base/drms/scripts/NetDRMS.sql
+++ base/drms/scripts/NetDRMS.sql
@@ -27,3 +27,11 @@ GRANT USAGE ON SCHEMA drms TO public;
 -- The PostgreSQL administrator account (e.g., "production") is actually an alias for this user, which 
 -- has elevated privileges.
 CREATE USER SUMSADMIN;
+
+-- In order to read from table _jsoc.sl_table (which may not exist), there must be a 'jsoc' role
+-- that has SELECT permissions on this table. If no _jsoc.sl_table exists, there is no need
+-- for the jsoc role to exist, however its existence in the absence of a _jsoc.sl_table is
+-- innocuous, so we always create role jsoc.
+--
+-- The database function drms_replicated() runs a query to read from _jsoc.sl_table.
+CREATE ROLE jsoc WITH NOLOGIN;
