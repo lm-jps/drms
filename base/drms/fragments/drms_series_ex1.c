476045f043d30858097e65ff8eb7b00fafba3d0a
cvs-fast-export <cvs-fast-export>
2016-05-17 17:47:28 +0000

    Synthetic commit for incomplete tag globalhs_version_11

--- base/drms/fragments/drms_series_ex1.c
+++
@@ -1,9 +0,0 @@
-//To check for the existence of a series:
-int status = DRMS_SUCCESS;
-drms_series_exists(drms_env, seriesout, &status);
-if (status == DRMS_ERROR_UNKNOWNSERIES))
-{
-  fprintf(stderr, "Output series %s doesn't exist.\n", seriesout);
-  return 1;
-}
-
--- base/libs/misc/byteswap.h
+++
@@ -1,6 +0,0 @@
-#ifndef __BYTESWAP_H
-#define __BYTESWAP_H
-
-void byteswap(int size, int n, char *val);
-
-#endif
