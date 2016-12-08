09031cc14c39482aaaa345e4fda5b796d60247db
cvs-fast-export <cvs-fast-export>
2016-12-08 02:02:43 +0000

    Synthetic commit for incomplete tag globalhs_version_14

--- base/drms/scripts/drms_whitelist.sql
+++
@@ -1,4 +0,0 @@
-# Create the whitelist table. This is a cache that contains the whitelist file data:
-CREATE TABLE drms.whitelist(seriesname text NOT NULL);
-ALTER TABLE drms.whitelist ADD CONSTRAINT whitelist_pkey PRIMARY KEY (seriesname);
-GRANT SELECT, INSERT, DELETE ON drms.whitelist TO PUBLIC;
