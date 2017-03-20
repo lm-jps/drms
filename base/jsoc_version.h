5dc76f46fe1279a73b608ad1f26f0604fcb5a790
arta <arta>
2017-03-20 21:01:58 +0000

    Set the release versions of the version macros for the 9.0 release.

--- base/jsoc_version.h
+++ base/jsoc_version.h
@@ -13,8 +13,8 @@
 
 #define JSOC_VERSION_INCL 1
 
-#define jsoc_version "V9R0X"
-#define jsoc_vers_num (-900)
+#define jsoc_version "V9R0"
+#define jsoc_vers_num (900)
 
 
 static inline const char *jsoc_getversion(char *verstr, int size, int *isdev)
