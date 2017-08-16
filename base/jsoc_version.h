e50270d7fdedcdd1f7481a3736396bd0101d0136
cvs-fast-export <cvs-fast-export>
2017-08-16 19:15:49 +0000

    Synthetic commit for incomplete tag Ver_9-1

--- base/jsoc_version.h
+++ base/jsoc_version.h
@@ -13,8 +13,8 @@
 
 #define JSOC_VERSION_INCL 1
 
-#define jsoc_version "V9R1X"
-#define jsoc_vers_num (-901)
+#define jsoc_version "V9R1"
+#define jsoc_vers_num (901)
 
 
 static inline const char *jsoc_getversion(char *verstr, int size, int *isdev)
