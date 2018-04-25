b59383e59faae24f7a4e354e56aea85f5127180d
arta <arta>
2018-04-25 19:36:06 +0000

    use original segment segnum

--- base/drms/scripts/drms_followsegmentlink.sql
+++ base/drms/scripts/drms_followsegmentlink.sql
@@ -22,28 +22,29 @@ DECLARE
     linkedseries varchar;
     linkedsegment varchar;
     linkmaprecord RECORD;
+    segnum int8;
 BEGIN
     -- origsegment is needed in the case that the caller provides NULL for datasegment (requesting information about all segments in a series)
     FOR linkmaprecord IN SELECT S.segment, S.linkedseries, S.linkedsegment FROM drms_linkedsegment(dataseries, datasegment) AS S LOOP
         origsegment := linkmaprecord.segment;
         linkedseries := linkmaprecord.linkedseries;
         linkedsegment := linkmaprecord.linkedsegment;
+        
+        SELECT regexp_split_to_array(dataseries, E'\\.') INTO parsed;
+        namespace := parsed[1];
+        relation := parsed[2];
 
         LOOP
-            raise notice '%', linkedseries;
-
             IF linkedseries IS NULL THEN
                 -- not a linked series
-                SELECT regexp_split_to_array(dataseries, E'\\.') INTO parsed;
-                namespace := parsed[1];
-                relation := parsed[2];
                 -- return original series and segment name so the caller can locate the information by input values easily
                 RETURN QUERY EXECUTE 'SELECT seriesname::varchar AS series, segmentname::varchar AS segment, type::varchar AS datatype, segnum::int8 AS segnum, scope::varchar, naxis::int8 AS numaxes, axis::varchar AS dimensions, unit::varchar, protocol::varchar, description FROM ' || quote_ident(namespace) || '.drms_segment WHERE lower(seriesname) = $1 AND lower(segmentname) = $2' USING lower(dataseries), lower(origsegment);
                 EXIT;
             ELSE
                 -- linked series
-                raise notice 'link query %', 'SELECT ' || quote_literal(dataseries) || '::varchar AS series, ' || quote_literal(origsegment) || '::varchar AS segment, datatype, segnum, scope, numaxes, dimensions, unit, protocol, description FROM drms_followsegmentlink(' || quote_literal(linkedseries) || ',' || quote_literal(linkedsegment) || ')';
-                RETURN QUERY EXECUTE 'SELECT ' || quote_literal(dataseries) || '::varchar AS series, ' || quote_literal(origsegment) || '::varchar AS segment, datatype, segnum, scope, numaxes, dimensions, unit, protocol, description FROM drms_followsegmentlink(' || quote_literal(linkedseries) || ',' || quote_literal(linkedsegment) || ')';
+                -- must return original segment number here
+                EXECUTE 'SELECT segnum::int8 FROM ' || namespace || '.drms_segment WHERE lower(seriesname) = lower(' || quote_literal(dataseries) || ') AND lower(segmentname) = lower(' || quote_literal(origsegment) || ')' INTO segnum;
+                RETURN QUERY EXECUTE 'SELECT ' || quote_literal(dataseries) || '::varchar AS series, ' || quote_literal(origsegment) || '::varchar AS segment, datatype, ' || quote_literal(segnum) || '::int8 AS segnum, scope, numaxes, dimensions, unit, protocol, description FROM drms_followsegmentlink(' || quote_literal(linkedseries) || ',' || quote_literal(linkedsegment) || ')';
                 EXIT;
             END IF;
         END LOOP;
