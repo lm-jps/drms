5e2dc87d7283bf18ea59904f58a65ffa2cb3b3f3
arta <arta>
2020-09-04 15:50:36 +0000

    change primary key of the pending requests table (we are now allowing certain people multiple reqs at the same time); add a new table, jsoc.export_pending_max_reqs, that contains a list of people exempt from the 1-request-at-a-time limit (and the number of simultaneous requests they are allowed)

--- base/export/scripts/init-db.sql
+++ base/export/scripts/init-db.sql
@@ -7,7 +7,7 @@ CREATE TABLE jsoc.export_pending_reqs
   ip_address   text not null,
   request_id   text,
   start_time   timestamp with time zone,
-  PRIMARY KEY (address)
+  PRIMARY KEY (address, request_id)
 );
 
 -- the export-system code will search on both address and ip_address
@@ -17,3 +17,16 @@ CREATE INDEX export_pending_reqs_ip_address ON jsoc.export_pending_reqs (ip_addr
 GRANT SELECT ON jsoc.export_pending_reqs TO public;
 GRANT INSERT ON jsoc.export_pending_reqs TO public;
 GRANT DELETE ON jsoc.export_pending_reqs TO public;
+
+
+-- as the netdrms production user, run this on each export database
+DROP TABLE IF EXISTS jsoc.export_pending_max_reqs;
+
+CREATE TABLE jsoc.export_pending_max_reqs
+(
+  address      text not null,
+  max_requests int CHECK (max_requests >= 0),
+  PRIMARY KEY  (address)
+);
+
+GRANT SELECT ON jsoc.export_pending_max_reqs TO public;
