ed3cfb0535ff124c96f770d958307fefcc5fc4c4
arta <arta>
2021-07-21 16:36:42 +0000

    Makes various object properties private; adds support for logging to file objects/streams

--- base/libs/py/drms_utils/log.py
+++ base/libs/py/drms_utils/log.py
@@ -3,6 +3,7 @@
 from argparse import Action as ApAction
 from enum import Enum
 import inspect
+from io import IOBase
 import logging
 import os
 
@@ -21,30 +22,35 @@ class Formatter(logging.Formatter):
 class Log(object):
     """Manage a logfile."""
     def __init__(self, file, level, formatter):
-        self.fileName = file
-        self.log = logging.getLogger()
-        self.log.setLevel(level)
-        self.fileHandler = logging.FileHandler(file)
-        self.fileHandler.setLevel(level)
-        self.fileHandler.setFormatter(formatter)
-        self.log.addHandler(self.fileHandler)
+        if isinstance(file, IOBase):
+            self._file_name = None
+            self._handler = logging.StreamHandler(file)
+        else:
+            self._file_name = file
+            self._handler = logging.FileHandler(file)
+
+        self._log = logging.getLogger()
+        self._log.setLevel(level)
+        self._handler.setLevel(level)
+        self._handler.setFormatter(formatter)
+        self._log.addHandler(self._handler)
 
     def close(self):
-        if self.log:
-            if self.fileHandler:
-                self.log.removeHandler(self.fileHandler)
-                self.fileHandler.flush()
-                self.fileHandler.close()
-                self.fileHandler = None
-            self.log = None
+        if self._log:
+            if self._handler:
+                self._log.removeHandler(self._handler)
+                self._handler.flush()
+                self._handler.close()
+                self._handler = None
+            self._log = None
 
     def flush(self):
-        if self.log and self.fileHandler:
-            self.fileHandler.flush()
+        if self._log and self._handler:
+            self._handler.flush()
 
     def getLevel(self):
         # Hacky way to get the level - make a dummy LogRecord
-        logRecord = self.log.makeRecord(self.log.name, self.log.getEffectiveLevel(), None, '', '', None, None)
+        logRecord = self._log.makeRecord(self._log.name, self._log.getEffectiveLevel(), None, '', '', None, None)
         return logRecord.levelname
 
     def __prependFrameInfo(self, msg):
@@ -52,34 +58,34 @@ class Log(object):
         return os.path.basename(fileName) + ':' + str(lineNo) + ': ' + msg
 
     def write_debug(self, text):
-        if self.log:
+        if self._log:
             for line in text:
-                self.log.debug(self.__prependFrameInfo(line))
-            self.fileHandler.flush()
+                self._log.debug(self.__prependFrameInfo(line))
+            self._handler.flush()
 
     def write_info(self, text):
-        if self.log:
+        if self._log:
             for line in text:
-                self.log.info(self.__prependFrameInfo(line))
-        self.fileHandler.flush()
+                self._log.info(self.__prependFrameInfo(line))
+        self._handler.flush()
 
     def write_warning(self, text):
-        if self.log:
+        if self._log:
             for line in text:
-                self.log.warning(self.__prependFrameInfo(line))
-            self.fileHandler.flush()
+                self._log.warning(self.__prependFrameInfo(line))
+            self._handler.flush()
 
     def write_error(self, text):
-        if self.log:
+        if self._log:
             for line in text:
-                self.log.error(self.__prependFrameInfo(line))
-            self.fileHandler.flush()
+                self._log.error(self.__prependFrameInfo(line))
+            self._handler.flush()
 
     def write_critical(self, text):
-        if self.log:
+        if self._log:
             for line in text:
                 self.log.critical(self.__prependFrameInfo(line))
-            self.fileHandler.flush()
+            self._handler.flush()
 
 class LogLevelAction(ApAction):
     def __call__(self, parser, namespace, value, option_string=None):
