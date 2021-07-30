c1e7f6ae325b4bcfcd8efcf55a60a5525939ead1
arta <arta>
2021-07-30 17:12:45 +0000

    Fixes typos in instance variables; corrects missing 'self' when referring to an instance method

--- base/libs/py/drms_utils/statuscode.py
+++ base/libs/py/drms_utils/statuscode.py
@@ -8,13 +8,12 @@ class StatusCode(Enum):
     # do not define enum members here - you cannot subclass an Enum class if the parent has members
     def __new__(cls, value, name):
         member = object.__new__(cls)
-        member._value_ = value
+        member._value = value
         member._fullname = name
         return member
 
     def __int__(self):
-        return self.value
+        return self._value
 
-    @property
     def fullname(self, **kwargs):
-        member._fullname.format(**kwargs)
+        self._fullname.format(**kwargs)
