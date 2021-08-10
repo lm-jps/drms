c0d23d6f32034471e28eb479deac94e1b5e3d111
arta <arta>
2021-08-10 18:04:59 +0000

    Defines a class variable, `_all_members` that contains all enum instances for that class; implements the `_missing_` method; when called (because the enum code will use the full tuple value as the key when searching for the corresponding enum object), `_all_members` is searched using only the str(enum int value)

--- base/libs/py/drms_utils/statuscode.py
+++ base/libs/py/drms_utils/statuscode.py
@@ -5,15 +5,25 @@ from enum import Enum
 __all__ = [ 'StatusCode' ]
 
 class StatusCode(Enum):
-    # do not define enum members here - you cannot subclass an Enum class if the parent has members
-    def __new__(cls, value, name):
+    # do not define enum members here - you cannot subclass an Enum class if the parent has members; members are 2-tuples: <int_value>, <description>, as in
+    # REQUEST_COMPLETE = (0, 'request has been completely processed')
+    def __new__(cls, int_value, description):
         member = object.__new__(cls)
-        member._value = value
-        member._fullname = name
+        member._int_value = int_value
+        member._description = description
+
+        if not hasattr(cls, '_all_members'):
+            cls._all_members = {}
+        cls._all_members[str(int_value)] = member
+
         return member
 
     def __int__(self):
-        return self._value
+        return self._int_value
 
-    def fullname(self, **kwargs):
-        return self._fullname.format(**kwargs)
+    def description(self, **kwargs):
+        return self._description.format(**kwargs) # 'request has been completely processed'
+
+    @classmethod
+    def _missing_(cls, int_value):
+        return cls._all_members[str(int_value)]
