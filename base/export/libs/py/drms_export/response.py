7c7f0fc12490af9ea89cbc07647b823cb3e91fdd
arta <arta>
2021-08-10 18:08:38 +0000

    Adds support for returning an ExportStatusCode in the 'status' property of the returned `dict` response; also replaces `msg` with `error_message` as the object member variable when the object is an ErrorResponse

--- base/export/libs/py/drms_export/response.py
+++ base/export/libs/py/drms_export/response.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
-from json import dumps, loads
+from copy import deepcopy
 from collections import namedtuple
+from json import dumps, loads
 
 __all__ = [ 'Response' ]
 
 class Response(object):
-    def __init__(self, *, status_code, msg=None, **kwargs):
+    def __init__(self, *, status_code, **kwargs):
         self._status_code = status_code
-        self._msg = msg
         self._kwargs = kwargs
         self._json_obj_response = None
         self._json_response = None
         self._dict_response = None
 
     def __str__(self):
-        return self._msg
+        return str(self._status_code)
 
     def generate_json_obj(self):
         if self._json_obj_response is None:
@@ -24,24 +24,24 @@ class Response(object):
 
     def generate_json(self):
         if self._json_response is None:
-            self._json_response = dumps(self.generate_dict())
+            self.generate_dict()
+            working = deepcopy(self._dict_response)
+            working['status_code'] = int(self._dict_response['status_code']) # serialize for json
+            self._json_response = dumps(working)
         return self._json_response
 
     def generate_dict(self):
         if self._dict_response is None:
-            self._dict_response = { "status" : int(self._status_code), "msg" : self._msg }
+            self._dict_response = { 'status' : str(self._status_code), 'status_code' : self._status_code, 'status_description' : self._status_code.description(**self._kwargs) }
             self._dict_response.update(self._kwargs)
         return self._dict_response
 
     @classmethod
-    def generate_response(cls, *, status_code=None, msg=None, **kwargs):
+    def generate_response(cls, *, status_code=None, **kwargs):
         status_code_final = status_code if status_code is not None else cls._status_code
 
-        if msg is None:
-            msg = status_code_final.fullname(**kwargs)
-
-        return cls(status_code=status_code_final, msg=msg, **kwargs)
+        return cls(status_code=status_code_final, **kwargs)
 
 class ErrorResponse(Response):
-    def __init__(self, *, error_code, msg=None):
-        super().__init__(status_code=error_code, msg=msg)
+    def __init__(self, *, error_code, error_message=None):
+        super().__init__(status_code=error_code, error_message=None)
