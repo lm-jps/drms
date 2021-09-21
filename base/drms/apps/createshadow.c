56bcd134f3e1e32798e319a8bd2c6b9ce892f1e7
arta <arta>
2021-09-21 14:16:38 +0000

    Adds information/caveat about providing the `tname` argument

--- base/drms/apps/createshadow.c
+++ base/drms/apps/createshadow.c
@@ -17,22 +17,22 @@ typedef enum
 char *module_name = "createshadow";
 
 /* Command line parameter values. */
-ModuleArgs_t module_args[] = 
+ModuleArgs_t module_args[] =
 {
     {ARG_STRING, kSeries, "", "The series for which a shadow table is to be created."},
-    {ARG_STRING, kTablename, kUndefined, "Optional - the name of the shadow-table to create. This is to be used for debugging since there is only one name that DRMS will recognize (<series name>_shadow). "},
+    {ARG_STRING, kTablename, kUndefined, "Optional - the name of the shadow-table to create. This is to be used for debugging only. There is only one table name that DRMS recognizes (<series name>_shadow). If a value for tname is provided, then no trigger will be installed on the <series name> table. The trigger function references <series name>_shadow, so installing it without creating the offical shadow table <series name>_shadow will result in the trigger code failing."},
     {ARG_END}
 };
 
 
-int DoIt(void) 
+int DoIt(void)
 {
     CSError_t err = kCSErr_Success;
     int drmsstat = DRMS_SUCCESS;
-    
+
     const char *series = cmdparams_get_str(&cmdparams, kSeries, &drmsstat);
     const char *tname = cmdparams_get_str(&cmdparams, kTablename, &drmsstat);
-    
+
     if (drmsstat != DRMS_SUCCESS)
     {
         err = kCSErr_BadArg;
@@ -43,10 +43,10 @@ int DoIt(void)
         {
             tname = NULL;
         }
-        
+
         drms_series_setcreateshadows(drms_env, NULL);
         drmsstat = drms_series_createshadow(drms_env, series, tname);
-        
+
         if (drmsstat != DRMS_SUCCESS)
         {
             if (drmsstat == DRMS_ERROR_OUTOFMEMORY)
@@ -57,12 +57,12 @@ int DoIt(void)
             {
                 err = kCSErr_BadQuery;
             }
-            else 
+            else
             {
                 err = kCSErr_CantCreateShadow;
             }
         }
     }
-    
+
     return err;
 }
