878cddfa65e556498dbeae3d349f4a26615e0d48
arta <arta>
2021-12-02 20:57:10 +0000

    Adds required db_host argument to all requests (except for parse-specification request)

--- base/export/scripts/export_client.py
+++ base/export/scripts/export_client.py
@@ -29,30 +29,30 @@ if __name__ == "__main__":
             process_request(message, connection)
 
             # 2. series list
-            message = { 'request_type' : 'series_list', 'series_regex' : 'm_720s' }
+            message = { 'request_type' : 'series_list', 'series_regex' : 'm_720s', 'db_host' : 'hmidb' }
             process_request(message, connection)
 
             # 2. series info
-            message = { 'request_type' : 'series_info', 'series' : 'hmi.v_45s' }
+            message = { 'request_type' : 'series_info', 'series' : 'hmi.v_45s', 'db_host' : 'hmidb2' }
             process_request(message, connection)
 
             # 3. record info
-            message = { 'request_type' : 'record_info', 'specification' : 'hmi.m_720s[2015.2.2/96m]', 'keywords' : [ 't_rec', 't_sel' ], 'segments' : [ 'magnetogram' ], 'record_info' : False, 'number_records' : 128 }
+            message = { 'request_type' : 'record_info', 'specification' : 'hmi.m_720s[2015.2.2/96m]', 'keywords' : [ 't_rec', 't_sel' ], 'segments' : [ 'magnetogram' ], 'record_info' : False, 'number_records' : 128, 'db_host' : 'hmidb' }
             process_request(message, connection)
 
             # 4. premium export
-            message = { 'request_type' : 'premium_export', 'address' : 'arta@sun.stanford.edu', 'specification' : 'hmi.v_720s[2015.3.12/24m]', 'method' : 'url', 'requestor' : 'art', 'processing' : None, 'file_format' : 'fits', 'number_records' : 1 }
+            message = { 'request_type' : 'premium_export', 'address' : 'arta@sun.stanford.edu', 'specification' : 'hmi.v_720s[2015.3.12/24m]', 'method' : 'url', 'requestor' : 'art', 'processing' : None, 'file_format' : 'fits', 'number_records' : 1, 'db_host' : 'hmidb2' }
             process_request(message, connection)
 
             # 5. mini export
-            message = { 'request_type' : 'mini_export', 'address' : 'arta@sun.stanford.edu', 'specification' : 'hmi.v_720s[2017.12.5/96m]', 'requestor' : 'art', 'file_name_format' : '{seriesname}.{recnum:%lld}.{segment}', 'number_records' : 2 }
+            message = { 'request_type' : 'mini_export', 'address' : 'arta@sun.stanford.edu', 'specification' : 'hmi.v_720s[2017.12.5/96m]', 'requestor' : 'art', 'file_name_format' : '{seriesname}.{recnum:%lld}.{segment}', 'number_records' : 2, 'db_host' : 'hmidb2' }
             process_request(message, connection)
 
             # 6. streamed export
-            message = { 'request_type' : 'streamed_export', 'specification' : 'hmi.v_720s[2015.8.5]' }
+            message = { 'request_type' : 'streamed_export', 'specification' : 'hmi.v_720s[2015.8.5]', 'db_host' : 'hmidb' }
 
             # 7. request status
-            message = { 'request_type' : 'export_status', 'address' : 'arta@sun.stanford.edu', 'request_id' : 'JSOC_20211019_1665' }
+            message = { 'request_type' : 'export_status', 'address' : 'arta@sun.stanford.edu', 'request_id' : 'JSOC_20211019_1665', 'db_host' : 'hmidb2' }
             process_request(message, connection)
 
             # quit
